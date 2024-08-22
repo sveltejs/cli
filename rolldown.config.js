d4c011c0666cfe6eea03489214007a02b7b5c7ab
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-08-22 13:28:06 -0400

    add rolldown

--- rolldown.config.js
+++ rolldown.config.js
@@ -1,21 +1,16 @@
 import fs from 'node:fs';
 import path from 'node:path';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
-import dynamicImportVars from '@rollup/plugin-dynamic-import-vars';
-import { preserveShebangs } from 'rollup-plugin-preserve-shebangs';
-import dts from 'unplugin-isolated-decl/rollup';
-import esbuild from 'rollup-plugin-esbuild';
+import dts from 'unplugin-isolated-decl/rolldown';
 import { execSync } from 'node:child_process';
+import { isBuiltin } from 'node:module';
 
 /** @import { Package } from "./packages/core/utils/common.js" */
-/** @import { Plugin, RollupOptions } from "rollup" */
+/** @import { Plugin, RolldownOptions } from "rolldown" */
 /** @typedef {Package & { peerDependencies: Record<string, string> }} PackageJson */
 
 /**
  * @param {string} project
- * @returns {RollupOptions}
+ * @returns {RolldownOptions}
  */
 function getConfig(project) {
 	const inputs = [`./packages/${project}/index.ts`];
@@ -24,50 +19,66 @@ function getConfig(project) {
 	if (project === 'core') inputs.push(`./packages/${project}/internal.ts`);
 
 	const projectRoot = path.resolve(path.join(outDir, '..'));
-	fs.rmSync(outDir, { force: true, recursive: true });
+	// fs.rmSync(outDir, { force: true, recursive: true });
 
 	/** @type {PackageJson} */
 	const pkg = JSON.parse(fs.readFileSync(path.join(projectRoot, 'package.json'), 'utf8'));
 	const externalDeps = getExternalDeps(pkg);
 
-	// always externalizes `@svelte-cli/core` and any deps that are `dependencies` or `peerDependencies`
-	const external = [/@svelte-cli\/core\w*/g, ...externalDeps];
+	const external = [
+		// As of now, Rolldown doesn't transform cjs `require` calls into esm imports, so we need to
+		// manually externalize packages that use cjs.
+		/^commander\w*/g,
+		/^picocolors\w*/g,
+		/^postcss\w*/g,
+		/^recast\w*/g,
+		// TODO: investigate why Rolldown is transforming dedent into malformed code
+		/^dedent\w*/g,
+		/^@svelte-cli\/core\w*/g
+	];
 
-	/** @type {Plugin | undefined} */
-	let buildCliTemplatesPlugin;
+	/** @type {Plugin[]} */
+	const plugins = [
+		{
+			name: 'resolve-node-modules',
+			resolveId(id) {
+				const shouldExternal = externalDeps.has(id) || isBuiltin(id);
+				if (shouldExternal) {
+					return { id, external: true };
+				}
+			}
+		}
+	];
+
+	if ('exports' in pkg) plugins.push(dts());
+
 	if (project === 'create') {
-		// This custom rollup plugin is used to build the templates
-		// and place them inside the dist folder after every rollup build.
-		// This is necessary because rollup clears the output directory and
+		// This custom rolldown plugin is used to build the templates
+		// and place them inside the dist folder after every rolldown build.
+		// This is necessary because rolldown clears the output directory and
 		// thus also removes the template files
-		buildCliTemplatesPlugin = {
+		plugins.push({
 			name: 'build-cli-templates',
-			writeBundle() {
+			buildEnd() {
 				console.log('building templates');
 				execSync('node scripts/build-templates.js', { cwd: path.resolve('packages', 'create') });
 				console.log('finished building templates');
 			}
-		};
+		});
 	}
 
 	return {
 		input: inputs,
+		platform: 'node',
 		output: {
 			dir: outDir,
+			sourcemap: true,
 			format: 'esm',
-			sourcemap: true
+			// adds the shebang to the top of the bundle
+			banner: project === 'cli' ? '#!/usr/bin/env node' : undefined
 		},
 		external,
-		plugins: [
-			preserveShebangs(),
-			'exports' in pkg && dts(),
-			esbuild({ tsconfig: 'tsconfig.json', sourceRoot: projectRoot }),
-			nodeResolve({ preferBuiltins: true, rootDir: projectRoot }),
-			commonjs(),
-			json(),
-			dynamicImportVars(),
-			buildCliTemplatesPlugin
-		]
+		plugins
 	};
 }
 
