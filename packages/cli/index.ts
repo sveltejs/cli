6b5b33dd82e3f2910287574c0d58fb0504232f26
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-08-30 15:03:34 -0400

    update entry

--- packages/cli/index.ts
+++ packages/cli/index.ts
@@ -1,68 +1,10 @@
 #!/usr/bin/env node
 
-import { remoteControl, executeAdders, prompts } from '@svelte-cli/core/internal';
 import pkg from './package.json';
-import type {
-	AdderDetails,
-	AddersToApplySelectorParams,
-	ExecutingAdderInfo,
-	Question
-} from '@svelte-cli/core';
-import { adderCategories, categories, adderIds, type CategoryKeys } from '@svelte-cli/config';
-import { getAdderDetails } from '@svelte-cli/adders';
+import { program } from 'commander';
+import { add } from './commands/add.js';
+import { create } from './commands/create.js';
 
-void executeCli();
-
-async function executeCli() {
-	remoteControl.enable();
-
-	const adderDetails: Array<AdderDetails<Record<string, Question>>> = [];
-
-	for (const adderName of adderIds) {
-		const adder = await getAdderDetails(adderName);
-		adderDetails.push({ config: adder.config, checks: adder.checks });
-	}
-
-	const executingAdderInfo: ExecutingAdderInfo = {
-		name: pkg.name,
-		version: pkg.version
-	};
-
-	await executeAdders(adderDetails, executingAdderInfo, undefined, selectAddersToApply);
-
-	remoteControl.disable();
-}
-
-type AdderOption = { value: string; label: string; hint: string };
-async function selectAddersToApply({ projectType, addersMetadata }: AddersToApplySelectorParams) {
-	const promptOptions: Record<string, AdderOption[]> = {};
-
-	for (const [categoryId, adderIds] of Object.entries(adderCategories)) {
-		const categoryDetails = categories[categoryId as CategoryKeys];
-		const options: AdderOption[] = [];
-		const adders = addersMetadata.filter((x) => adderIds.includes(x.id));
-
-		for (const adder of adders) {
-			// if we detected a kit project, and the adder is not available for kit, ignore it.
-			if (projectType === 'kit' && !adder.environments.kit) continue;
-			// if we detected a svelte project, and the adder is not available for svelte, ignore it.
-			if (projectType === 'svelte' && !adder.environments.svelte) continue;
-
-			options.push({
-				label: adder.name,
-				value: adder.id,
-				hint: adder.website?.documentation || ''
-			});
-		}
-
-		if (options.length > 0) {
-			promptOptions[categoryDetails.name] = options;
-		}
-	}
-	const selectedAdders = await prompts.groupedMultiSelectPrompt(
-		'What would you like to add to your project?',
-		promptOptions
-	);
-
-	return selectedAdders;
-}
+program.name(pkg.name).version(pkg.version, '-v');
+program.addCommand(create).addCommand(add);
+program.parse();
