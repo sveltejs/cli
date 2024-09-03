03124704aff834e5c5a6340a7ee564d0a3fa7b7c
Stibbs <matthew@stibbard.io>
2024-09-03 10:27:13 +1000

    Apply suggestions from benmccann and manuel3108
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>
    Co-authored-by: Manuel <30698007+manuel3108@users.noreply.github.com>

--- packages/adders/_config/categories.ts
+++ packages/adders/_config/categories.ts
@@ -7,9 +7,13 @@ export type CategoryInfo = {
 };
 
 <<<<<<< HEAD:packages/adders/_config/categories.ts
+<<<<<<< HEAD:packages/adders/_config/categories.ts
 =======
 export type CategoryKeys = 'codeQuality' | 'css' | 'db' | 'testing' | 'additional' | 'baas';
 >>>>>>> a19c799 (add baas category):packages/config/categories.ts
+=======
+export type CategoryKeys = 'codeQuality' | 'css' | 'db' | 'testing' | 'additional';
+>>>>>>> f47d2f4 (Apply suggestions from benmccann and manuel3108):packages/config/categories.ts
 export type CategoryDetails = Record<CategoryKeys, CategoryInfo>;
 
 export type AdderCategories = Record<CategoryKeys, string[]>;
