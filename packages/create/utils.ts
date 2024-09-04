bc4dfdb53f268174f58df21a36df4b8bcaaacfe1
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-09-04 19:08:25 -0400

    unnecessary

--- packages/create/utils.ts
+++ packages/create/utils.ts
@@ -44,21 +44,3 @@ export function dist(path: string): string {
 		new URL(`./${!insideDistFolder ? 'dist/' : ''}${path}`, import.meta.url).href
 	);
 }
-
-export const package_manager: string = get_package_manager() || 'npm';
-
-/**
- * Supports npm, pnpm, Yarn, cnpm, bun and any other package manager that sets the
- * npm_config_user_agent env variable.
- * Thanks to https://github.com/zkochan/packages/tree/main/which-pm-runs for this code!
- */
-function get_package_manager() {
-	if (!process.env.npm_config_user_agent) {
-		return undefined;
-	}
-	const user_agent = process.env.npm_config_user_agent;
-	const pm_spec = user_agent.split(' ')[0];
-	const separator_pos = pm_spec.lastIndexOf('/');
-	const name = pm_spec.substring(0, separator_pos);
-	return name === 'npminstall' ? 'cnpm' : name;
-}
