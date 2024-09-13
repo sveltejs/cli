d9b61d4917308be7cbdb6457dad086f0b9393940
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-09-13 12:54:15 -0400

    simplify with tinyexec

--- packages/cli/common.ts
+++ packages/cli/common.ts
@@ -1,11 +1,11 @@
 import pc from 'picocolors';
 import pkg from './package.json';
+import { exec } from 'tinyexec';
 import * as p from '@svelte-cli/clack-prompts';
 import { detect, AGENTS, type Agent, type AgentName } from 'package-manager-detector';
 import { COMMANDS, constructCommand } from 'package-manager-detector/commands';
 import type { AdderWithoutExplicitArgs } from '@svelte-cli/core';
 import type { Argument, HelpConfiguration, Option } from 'commander';
-import { spawn, type ChildProcess } from 'node:child_process';
 
 export const helpConfig: HelpConfiguration = {
 	argumentDescription: formatDescription,
@@ -40,46 +40,9 @@ export async function runCommand(action: MaybePromise) {
 	}
 }
 
-export async function executeCli(
-	command: string,
-	commandArgs: string[],
-	cwd: string,
-	options?: {
-		onData?: (data: string, program: ChildProcess, resolve: (value?: any) => any) => void;
-		stdio?: 'pipe' | 'inherit';
-		env?: Record<string, string>;
-	}
-): Promise<any> {
-	const stdio = options?.stdio ?? 'pipe';
-	const env = options?.env ?? process.env;
-
-	const program = spawn(command, commandArgs, { stdio, shell: true, cwd, env });
-
-	return await new Promise((resolve, reject) => {
-		let errorText = '';
-		program.stderr?.on('data', (data: Buffer) => {
-			const value = data.toString();
-			errorText += value;
-		});
-
-		program.stdout?.on('data', (data: Buffer) => {
-			const value = data.toString();
-			options?.onData?.(value, program, resolve);
-		});
-
-		program.on('exit', (code) => {
-			if (code == 0) {
-				resolve(undefined);
-			} else {
-				reject(new Error(errorText));
-			}
-		});
-	});
-}
-
 export async function formatFiles(cwd: string, paths: string[]): Promise<void> {
-	await executeCli('npx', ['prettier', '--write', '--ignore-unknown', ...paths], cwd, {
-		stdio: 'pipe'
+	await exec('npx', ['prettier', '--write', '--ignore-unknown', ...paths], {
+		nodeOptions: { cwd, stdio: 'pipe' }
 	});
 }
 
@@ -131,24 +94,22 @@ export function getUserAgent(): Agent | undefined {
 	if (!userAgent) return undefined;
 	const pmSpec = userAgent.split(' ')[0];
 	const separatorPos = pmSpec.lastIndexOf('/');
-	const name = pmSpec.substring(0, separatorPos);
-	return name as Agent;
+	const name = pmSpec.substring(0, separatorPos) as Agent;
+	return AGENTS.includes(name) ? name : undefined;
 }
 
 async function installDependencies(command: string, args: string[], workingDirectory: string) {
 	try {
-		await executeCli(command, args, workingDirectory);
+		await exec(command, args, { nodeOptions: { cwd: workingDirectory } });
 	} catch (error) {
 		const typedError = error as Error;
 		throw new Error('unable to install dependencies: ' + typedError.message);
 	}
 }
 
-export type ProjectType = 'svelte' | 'kit';
-
 export function getGlobalPreconditions(
 	cwd: string,
-	projectType: ProjectType,
+	projectType: 'svelte' | 'kit',
 	adders: AdderWithoutExplicitArgs[]
 ) {
 	return {
@@ -157,8 +118,6 @@ export function getGlobalPreconditions(
 			{
 				name: 'clean working directory',
 				run: async () => {
-					let outputText = '';
-
 					try {
 						// If a user has pending git changes the output of the following command will list
 						// all files that have been added/modified/deleted and thus the output will not be empty.
@@ -166,13 +125,9 @@ export function getGlobalPreconditions(
 						// there are no pending changes. If the below command is run outside of a git repository,
 						// git will exit with a failing exit code, which will trigger the catch statement.
 						// also see https://remarkablemark.org/blog/2017/10/12/check-git-dirty/#git-status
-						await executeCli('git', ['status', '--short'], cwd, {
-							onData: (data) => {
-								outputText += data;
-							}
-						});
+						const { stdout } = await exec('git', ['status', '--short'], { nodeOptions: { cwd } });
 
-						if (outputText) {
+						if (stdout) {
 							return { success: false, message: 'Found modified files' };
 						}
 
