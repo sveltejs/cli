f30aff21373cd63f30bfecde4a49a27094d85ad9
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-09-14 12:16:18 -0400

    use TS modules instead

---
+++ community/unplugin-icons.ts
@@ -0,0 +1,13 @@
+import type { CommunityAdder } from '../packages/adders/_config/community';
+
+export default {
+	name: 'unplugin-icons',
+	id: 'unplugin-icons',
+	description: 'Access thousands of icons as components on-demand universally',
+	category: 'Icons',
+	npm: 'unplugin-icons-svelte-integration',
+	repo: 'owner-name/repo-name',
+	github: 'https://github.com/.../...',
+	website: 'https://www.npmjs.com/package/unplugin-icons',
+	logo: 'unplugin-icons.svg'
+} satisfies CommunityAdder;
