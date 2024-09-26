f4a45d3b61d5df20a39b9e239fac1d4fff035ad4
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-09-26 17:13:53 -0400

    add to core

---
+++ packages/core/tooling/js/index.ts
@@ -0,0 +1,8 @@
+export * as array from './array.ts';
+export * as object from './object.ts';
+export * as common from './common.ts';
+export * as functions from './function.ts';
+export * as imports from './imports.ts';
+export * as variables from './variables.ts';
+export * as exports from './exports.ts';
+export type { AstTypes } from '@svelte-cli/ast-tooling';
---
+++ packages/core/tooling/js/variables.ts
@@ -0,0 +1,45 @@
+import type { AstKinds, AstTypes } from '@svelte-cli/ast-tooling';
+
+export function declaration(
+	ast: AstTypes.Program | AstKinds.DeclarationKind,
+	kind: 'const' | 'let' | 'var',
+	name: string,
+	value: AstKinds.ExpressionKind
+): AstTypes.VariableDeclaration {
+	const declarations =
+		ast.type == 'Program'
+			? ast.body.filter((x): x is AstTypes.VariableDeclaration => x.type == 'VariableDeclaration')
+			: [ast as AstTypes.VariableDeclaration];
+	let declaration = declarations.find((x) => {
+		const declarator = x.declarations[0] as AstTypes.VariableDeclarator;
+		const identifier = declarator.id as AstTypes.Identifier;
+		return identifier.name == name;
+	});
+
+	if (declaration) return declaration;
+
+	declaration = {
+		type: 'VariableDeclaration',
+		kind,
+		declarations: [
+			{
+				type: 'VariableDeclarator',
+				id: {
+					type: 'Identifier',
+					name
+				},
+				init: value
+			}
+		]
+	};
+
+	return declaration;
+}
+
+export function identifier(name: string): AstTypes.Identifier {
+	const identifier: AstTypes.Identifier = {
+		type: 'Identifier',
+		name
+	};
+	return identifier;
+}
