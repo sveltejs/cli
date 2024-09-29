599186fcd56c1827dc92ca7b3ca6c28c292f1438
Manuel Serret <mserret99@gmail.com>
2024-09-29 09:35:43 +0200

    simplify

--- packages/cli/commands/migrate.ts
+++ packages/cli/commands/migrate.ts
@@ -1,8 +1,5 @@
 import { execSync } from 'node:child_process';
-import { exec } from 'tinyexec';
 import { Command } from 'commander';
-import { COMMANDS, constructCommand } from 'package-manager-detector';
-import * as common from '../common.js';
 
 export const migrate = new Command('migrate')
 	.description('A CLI for migrating Svelte(Kit) codebases')
@@ -10,17 +7,10 @@ export const migrate = new Command('migrate')
 	.configureHelp({
 		formatHelp() {
 			// we'll pass the responsibility of presenting the help menu over to `svelte-migrate`
-			execSync('npx svelte-migrate@latest --help', { stdio: 'inherit' });
+			execSync('npx --yes svelte-migrate@latest --help', { stdio: 'inherit' });
 			return '';
 		}
 	})
-	.action(async (migration) => {
-		const cwd = process.cwd();
-		const pm = await common.guessPackageManager(cwd);
-		const { command, args } = constructCommand(COMMANDS[pm].execute, [
-			'svelte-migrate@latest',
-			migration
-		])!;
-
-		await exec(command, args, { nodeOptions: { cwd, stdio: 'inherit' } });
+	.action((migration) => {
+		execSync(`npx --yes svelte-migrate@latest ${migration}`, { stdio: 'inherit' });
 	});
