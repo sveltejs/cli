3a7757616b73508d21b86f53d4a157e9840a72e7
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-10-02 13:22:34 -0400

    simplify categories

--- packages/adders/_config/categories.ts
+++ packages/adders/_config/categories.ts
@@ -1,47 +1,15 @@
-export type Category = 'codeQuality' | 'css' | 'db' | 'testing' | 'auth' | 'additional';
-export type CommunityCategory = 'community-category';
-
-export type CategoryInfo = {
-	name: string;
-	description: string;
-};
-
-export type CategoryDetails = Record<Category, CategoryInfo>;
-export type CommunityCategoryDetails = Record<CommunityCategory, CategoryInfo>;
+export type Category = (typeof categories)[number];
+export type CommunityCategory = (typeof communityCategories)[number];
 
 export type AdderCategories = Record<Category, string[]>;
 
-export const categories: CategoryDetails = {
-	codeQuality: {
-		name: 'Code Quality',
-		description: ''
-	},
-	testing: {
-		name: 'Testing',
-		description: ''
-	},
-	css: {
-		name: 'CSS',
-		description: 'Can be used to style your components'
-	},
-	db: {
-		name: 'Database',
-		description: ''
-	},
-	auth: {
-		id: 'auth',
-		name: 'Auth',
-		description: ''
-	},
-	additional: {
-		name: 'Additional functionality',
-		description: ''
-	}
-};
+export const categories = [
+	'Code Quality',
+	'Testing',
+	'CSS',
+	'Database',
+	'Auth',
+	'Additional Functionality'
+] as const;
 
-export const communityCategories: CommunityCategoryDetails = {
-	'community-category': {
-		name: 'Community category',
-		description: ''
-	}
-};
+export const communityCategories = ['Icon'] as const;
