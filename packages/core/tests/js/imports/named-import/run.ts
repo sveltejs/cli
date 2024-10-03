0af1308b464057e7b8e48c6c0fe21c72c9b30917
Manuel Serret <mserret99@gmail.com>
2024-10-03 15:13:31 +0200

    fix duplicated handle import

--- packages/core/tests/js/imports/named-import/run.ts
+++ packages/core/tests/js/imports/named-import/run.ts
@@ -3,4 +3,8 @@ import type { ScriptFileEditor } from '@svelte-cli/core';
 
 export function run({ ast }: ScriptFileEditor<any>): void {
 	imports.addNamed(ast, 'package', { namedOne: 'namedOne' }, false);
+
+	imports.addNamed(ast, '@sveltejs/kit', { Handle: 'Handle' }, false);
+	// adding the same import twice should not produce two imports
+	imports.addNamed(ast, '@sveltejs/kit', { Handle: 'Handle' }, false);
 }
