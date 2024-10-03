37a265609be5cf8a4a3ddeb03140142d7f8119f0
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-10-03 12:40:00 -0400

    add `check` command

---
+++ packages/cli/commands/check.ts
@@ -0,0 +1,47 @@
+import { createRequire } from 'node:module';
+import { execSync } from 'node:child_process';
+import pc from 'picocolors';
+import { Command } from 'commander';
+import { resolveCommand } from 'package-manager-detector/commands';
+import { getUserAgent } from '../common.ts';
+
+const require = createRequire(import.meta.url);
+
+export const check = new Command('check')
+	.description('a CLI for checking your Svelte code')
+	// flags that we'll want to pass to `svelte-check`
+	.allowUnknownOption(true)
+	.option('-C, --cwd <path>', 'path to working directory', process.cwd())
+	.configureHelp({
+		formatHelp() {
+			// we'll pass the responsibility of presenting the help menu over to `svelte-check`
+			runCheck(process.cwd(), ['--help']);
+			return '';
+		}
+	})
+	.action((options, check: Command) => {
+		const cwd: string = options.cwd;
+		const args: string[] = check.args;
+
+		runCheck(cwd, args);
+	});
+
+function runCheck(cwd: string, args: string[]) {
+	const pm = getUserAgent() ?? 'npm';
+
+	// validates that `svelte-check` is locally installed
+	try {
+		require.resolve('svelte-check', { paths: [cwd] });
+	} catch {
+		const cmd = resolveCommand(pm, 'add', ['svelte-check'])!;
+		console.error(
+			`'svelte-check' is not installed locally. Install it with: ${pc.bold(`${cmd.command} ${cmd.args.join(' ')}`)}`
+		);
+		process.exit(1);
+	}
+
+	// avoids printing the stack trace for `sv` when `svelte-check` exits with an error code
+	try {
+		execSync(`npx svelte-check ${args.join(' ')}`, { stdio: 'inherit', cwd });
+	} catch {}
+}
