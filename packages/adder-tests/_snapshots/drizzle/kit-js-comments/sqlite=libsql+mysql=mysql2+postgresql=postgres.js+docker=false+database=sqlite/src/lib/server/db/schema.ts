def7f0513ca4a9e7c9f42926ab3a102bb6f18c93
Manuel Serret <mserret99@gmail.com>
2024-10-04 18:48:25 +0200

    update snapshots

--- packages/adder-tests/_snapshots/drizzle/kit-js-comments/sqlite=libsql+mysql=mysql2+postgresql=postgres.js+docker=false+database=sqlite/src/lib/server/db/schema.ts
+++ packages/adder-tests/_snapshots/drizzle/kit-js-comments/sqlite=libsql+mysql=mysql2+postgresql=postgres.js+docker=false+database=sqlite/src/lib/server/db/schema.ts
@@ -2,6 +2,5 @@ import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 
 export const user = sqliteTable('user', {
     id: integer('id').primaryKey(),
-    name: text('name').notNull(),
     age: integer('age')
 });
--- packages/adder-tests/_snapshots/drizzle/kit-ts/sqlite=libsql+mysql=mysql2+postgresql=postgres.js+docker=true+database=postgresql/src/lib/server/db/schema.ts
+++ packages/adder-tests/_snapshots/drizzle/kit-ts/sqlite=libsql+mysql=mysql2+postgresql=postgres.js+docker=true+database=postgresql/src/lib/server/db/schema.ts
@@ -2,6 +2,5 @@ import { pgTable, serial, text, integer } from "drizzle-orm/pg-core";
 
 export const user = pgTable('user', {
     id: serial('id').primaryKey(),
-    name: text('name').notNull(),
     age: integer('age'),
 });
