54d74775564c280cedb7595efaa56d67b9f5077d
Manuel Serret <mserret99@gmail.com>
2024-10-05 19:40:18 +0200

    fix testing environment detection

--- packages/core/env.ts
+++ packages/core/env.ts
@@ -1,1 +1,2 @@
-export const TESTING: boolean = process.env.CI?.toLowerCase() === 'true';
+export const TESTING: boolean =
+	process.env.CI?.toLowerCase() === 'true' || process.env.NODE_ENV?.toLowerCase() === 'test';
