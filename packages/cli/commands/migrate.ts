d97aecd37a952c089de5f2ebeb68f61feb848142
CokaKoala <31664583+AdrianGonz97@users.noreply.github.com>
2024-10-05 13:55:23 -0400

    chore: Use user's PM when executing binary (#64)
    * update pm detector
    
    * use user's pm rather than `npx`

--- packages/cli/commands/migrate.ts
+++ packages/cli/commands/migrate.ts
@@ -1,5 +1,7 @@
 import { execSync } from 'node:child_process';
 import { Command } from 'commander';
+import { resolveCommand } from 'package-manager-detector';
+import { getUserAgent } from '../common.ts';
 
 export const migrate = new Command('migrate')
 	.description('a CLI for migrating Svelte(Kit) codebases')
@@ -8,13 +10,25 @@ export const migrate = new Command('migrate')
 	.configureHelp({
 		formatHelp() {
 			// we'll pass the responsibility of presenting the help menu over to `svelte-migrate`
-			execSync('npx --yes svelte-migrate@latest --help', { stdio: 'inherit' });
+			runMigrate(process.cwd(), ['--help']);
 			return '';
 		}
 	})
 	.action((migration, options) => {
-		execSync(`npx --yes svelte-migrate@latest ${migration}`, {
-			stdio: 'inherit',
-			cwd: options.cwd
-		});
+		runMigrate(options.cwd, [migration]);
 	});
+
+function runMigrate(cwd: string, args: string[]) {
+	const pm = getUserAgent() ?? 'npm';
+
+	// avoids printing the stack trace for `sv` when `svelte-migrate` exits with an error code
+	try {
+		const cmdArgs = ['svelte-migrate@latest', ...args];
+
+		// skips the download confirmation prompt for `npx`
+		if (pm === 'npm') cmdArgs.unshift('--yes');
+
+		const cmd = resolveCommand(pm, 'execute', cmdArgs)!;
+		execSync(`${cmd.command} ${cmd.args.join(' ')}`, { stdio: 'inherit', cwd });
+	} catch {}
+}
