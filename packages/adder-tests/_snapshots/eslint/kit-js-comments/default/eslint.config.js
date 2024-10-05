ab0566fb5546084ea9e617014d563a32b49af52f
Manuel Serret <mserret99@gmail.com>
2024-10-05 07:45:15 +0200

    update snapshots

--- packages/adder-tests/_snapshots/eslint/kit-js-comments/default/eslint.config.js
+++ packages/adder-tests/_snapshots/eslint/kit-js-comments/default/eslint.config.js
@@ -1,3 +1,4 @@
+import prettier from "eslint-config-prettier";
 import js from "@eslint/js";
 import svelte from "eslint-plugin-svelte";
 import globals from "globals";
@@ -8,6 +9,8 @@ export default [
   js.configs.recommended,
   ...ts.configs.recommended,
   ...svelte.configs["flat/recommended"],
+  prettier,
+  ...svelte.configs['flat/prettier'],
   {
     languageOptions: {
       globals: {
