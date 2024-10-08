ce115f493dbf342a6452c09c78f8bf2d4e6bf64a
Manuel <30698007+manuel3108@users.noreply.github.com>
2024-10-08 18:13:48 +0200

    Make children in layout non optional

--- packages/adders/supabase/config/adder.ts
+++ packages/adders/supabase/config/adder.ts
@@ -231,7 +231,7 @@ export const adder = defineAdderConfig({
 					`
 				);
 
-				addFromRawHtml(htmlAst.childNodes, '{@render children?.()}');
+				addFromRawHtml(htmlAst.childNodes, '{@render children()}');
 			}
 		},
 		{
