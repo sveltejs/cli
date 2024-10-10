ee1997f9b3292a22437823194c978bbf3093f03a
Manuel Serret <mserret99@gmail.com>
2024-10-10 20:14:42 +0200

    add jsdoc utility method

---
+++ packages/core/tests/js/common/jsdoc-comment/output.ts
@@ -0,0 +1,8 @@
+/**
+ * @param {import("$lib/paraglide/runtime").AvailableLanguageTag} newLanguage
+ */
+function switchToLanguage(newLanguage) {
+    const canonicalPath = i18n.route($page.url.pathname);
+    const localisedPath = i18n.resolveRoute(canonicalPath, newLanguage);
+    goto(localisedPath);
+}
---
+++ packages/core/tests/js/common/jsdoc-comment/run.ts
@@ -0,0 +1,10 @@
+import { array, common, variables } from '@svelte-cli/core/js';
+import type { AstTypes, ScriptFileEditor } from '@svelte-cli/core';
+
+export function run({ ast }: ScriptFileEditor<any>): void {
+	const functionDeclaration = ast.body[0] as AstTypes.FunctionDeclaration;
+
+	common.addJsDocComment(functionDeclaration, {
+		'import("$lib/paraglide/runtime").AvailableLanguageTag': 'newLanguage'
+	});
+}
