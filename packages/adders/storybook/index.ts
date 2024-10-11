bf14f4cd081b414c2c8c44dc710bc8f351a835fb
Manuel <30698007+manuel3108@users.noreply.github.com>
2024-10-11 17:31:36 +0200

    chore: simplify adder interface (#89)
    * simplify adder interface
    
    * move adder configs to parent directory
    
    * remove empty option files
    
    * rename adder file to index
    
    * rename logos
    
    * remove logo property
    
    * simplify options
    
    * few more tweaks
    
    * fix
    
    * fix `findRoot`
    
    ---------
    
    Co-authored-by: AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>

--- packages/adders/storybook/index.ts
+++ packages/adders/storybook/index.ts
@@ -1,3 +1,19 @@
-import { adder } from './config/adder.ts';
+import { defineAdder } from '@svelte-cli/core';
 
-export default adder;
+export default defineAdder({
+	id: 'storybook',
+	name: 'Storybook',
+	description: 'Build UIs without the grunt work',
+	environments: { kit: true, svelte: true },
+	documentation: 'https://storybook.js.org/docs/get-started',
+	options: {},
+	packages: [],
+	scripts: [
+		{
+			description: 'applies storybook',
+			args: ['storybook@latest', 'init', '--skip-install', '--no-dev'],
+			stdio: 'inherit'
+		}
+	],
+	files: []
+});
