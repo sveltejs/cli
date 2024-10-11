e24acd24713e84d8442bf455a22849da11c63791
Manuel Serret <mserret99@gmail.com>
2024-10-11 16:27:04 +0200

    rename adder file to index

--- packages/adders/storybook/index.ts
+++ packages/adders/storybook/index.ts
@@ -1,3 +1,20 @@
-import { adder } from './adder.ts';
+import { defineAdder } from '@svelte-cli/core';
 
-export default adder;
+export default defineAdder({
+	id: 'storybook',
+	name: 'Storybook',
+	description: 'Build UIs without the grunt work',
+	environments: { kit: true, svelte: true },
+	logo: './storybook.svg',
+	documentation: 'https://storybook.js.org/docs/get-started',
+	options: {},
+	packages: [],
+	scripts: [
+		{
+			description: 'applies storybook',
+			args: ['storybook@latest', 'init', '--skip-install', '--no-dev'],
+			stdio: 'inherit'
+		}
+	],
+	files: []
+});
