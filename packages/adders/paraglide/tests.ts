d2d13fb009c41c5cbb8f3757da09056d79c03ee7
Manuel Serret <mserret99@gmail.com>
2024-10-11 18:07:40 +0200

    fix

--- packages/adders/paraglide/tests.ts
+++ packages/adders/paraglide/tests.ts
@@ -1,6 +1,5 @@
 import { defineAdderTests } from '@svelte-cli/core';
-import { options } from './options.ts';
-
+import { options } from './index.ts';
 // e2e tests make no sense in this context
 
 export const tests = defineAdderTests({
