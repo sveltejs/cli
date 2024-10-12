62e8d152ca68c03cc202684a8da9240807105f66
Manuel Serret <mserret99@gmail.com>
2024-10-12 18:54:35 +0200

    fix command line parsing

--- packages/cli/commands/add/index.ts
+++ packages/cli/commands/add/index.ts
@@ -27,7 +27,8 @@ const AddersSchema = v.array(v.string());
 const AdderOptionFlagsSchema = v.object({
 	tailwindcss: v.optional(v.array(v.string())),
 	drizzle: v.optional(v.array(v.string())),
-	supabase: v.optional(v.array(v.string()))
+	supabase: v.optional(v.array(v.string())),
+	paraglide: v.optional(v.array(v.string()))
 });
 const OptionsSchema = v.strictObject({
 	cwd: v.string(),
