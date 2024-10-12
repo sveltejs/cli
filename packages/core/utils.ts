5ede289ec9318c878cfa92808b921c9e2986994b
Manuel Serret <mserret99@gmail.com>
2024-10-12 16:57:32 +0200

    re-type `createPrinter` function

--- packages/core/utils.ts
+++ packages/core/utils.ts
@@ -1,8 +1,12 @@
-export function createPrinter(
-	...conditions: boolean[]
-): Array<(content: string, alt?: string) => string> {
-	const printers = conditions.map((condition) => {
+type Printers<Condition extends Record<string, boolean>> = {
+	[K in keyof Condition]: (content: string, alt?: string) => string;
+};
+export function createPrinter<Condition extends Record<string, boolean>>(
+	conditions: Condition
+): Printers<Condition> {
+	const printers = Object.values(conditions).map((condition) => {
 		return (content: string, alt = '') => (condition ? content : alt);
-	});
+	}) as Printers<Condition>;
+
 	return printers;
 }
