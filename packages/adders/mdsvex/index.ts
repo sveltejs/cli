2d4cceb2dc1cf851b3984922b1a90537d2c3f01b
Manuel Serret <mserret99@gmail.com>
2024-10-22 19:56:11 +0200

    migrate mdsvex

--- packages/adders/mdsvex/index.ts
+++ packages/adders/mdsvex/index.ts
@@ -7,38 +7,36 @@ export default defineAdder({
 	environments: { svelte: true, kit: true },
 	homepage: 'https://mdsvex.pngwn.io',
 	options: {},
-	packages: [{ name: 'mdsvex', version: '^0.11.2', dev: true }],
-	files: [
-		{
-			name: () => 'svelte.config.js',
-			content: ({ content }) => {
-				const { ast, generateCode } = parseScript(content);
+	run: ({ sv }) => {
+		sv.devDependency('mdsvex', '^0.11.2');
 
-				imports.addNamed(ast, 'mdsvex', { mdsvex: 'mdsvex' });
+		sv.file('svelte.config.js', (content) => {
+			const { ast, generateCode } = parseScript(content);
 
-				const { value: exportDefault } = exports.defaultExport(ast, object.createEmpty());
+			imports.addNamed(ast, 'mdsvex', { mdsvex: 'mdsvex' });
 
-				// preprocess
-				let preprocessorArray = object.property(exportDefault, 'preprocess', array.createEmpty());
-				const isArray = preprocessorArray.type === 'ArrayExpression';
+			const { value: exportDefault } = exports.defaultExport(ast, object.createEmpty());
 
-				if (!isArray) {
-					const previousElement = preprocessorArray;
-					preprocessorArray = array.createEmpty();
-					array.push(preprocessorArray, previousElement);
-					object.overrideProperty(exportDefault, 'preprocess', preprocessorArray);
-				}
+			// preprocess
+			let preprocessorArray = object.property(exportDefault, 'preprocess', array.createEmpty());
+			const isArray = preprocessorArray.type === 'ArrayExpression';
 
-				const mdsvexCall = functions.call('mdsvex', []);
-				array.push(preprocessorArray, mdsvexCall);
-
-				// extensions
-				const extensionsArray = object.property(exportDefault, 'extensions', array.createEmpty());
-				array.push(extensionsArray, '.svelte');
-				array.push(extensionsArray, '.svx');
-
-				return generateCode();
+			if (!isArray) {
+				const previousElement = preprocessorArray;
+				preprocessorArray = array.createEmpty();
+				array.push(preprocessorArray, previousElement);
+				object.overrideProperty(exportDefault, 'preprocess', preprocessorArray);
 			}
-		}
-	]
+
+			const mdsvexCall = functions.call('mdsvex', []);
+			array.push(preprocessorArray, mdsvexCall);
+
+			// extensions
+			const extensionsArray = object.property(exportDefault, 'extensions', array.createEmpty());
+			array.push(extensionsArray, '.svelte');
+			array.push(extensionsArray, '.svx');
+
+			return generateCode();
+		});
+	}
 });
