ee918507301e4f2b9992c890b86aef89a55114a8
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-10-23 18:44:55 -0400

    `CI` isn't necessary and was just a placeholder

--- packages/cli/env.ts
+++ packages/cli/env.ts
@@ -1,4 +1,3 @@
 import process from 'node:process';
 
-export const TESTING: boolean =
-	process.env.CI?.toLowerCase() === 'true' || process.env.NODE_ENV?.toLowerCase() === 'test';
+export const TESTING: boolean = process.env.NODE_ENV?.toLowerCase() === 'test';
