857094054f6ecb07e3de9ebc8fe300a9317047cd
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-11-06 18:56:05 -0500

    remove old tests

--- packages/adders/lucia/tests.ts
+++
@@ -1,9 +0,0 @@
-import { defineAdderTests } from '@sveltejs/cli-core';
-import { options } from './index.ts';
-
-export const tests = defineAdderTests({
-	files: [],
-	options,
-	optionValues: [],
-	tests: []
-});
