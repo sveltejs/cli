809a72f5a261299d3fb150f1894b693a34075a56
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-11-06 20:41:07 -0500

    initial install helper impl

---
+++ packages/cli/lib/install.ts
@@ -0,0 +1,88 @@
+import { exec } from 'tinyexec';
+import { resolveCommand } from 'package-manager-detector';
+import type { Adder, Workspace, PackageManager, OptionValues, Question } from '@sveltejs/cli-core';
+import { createWorkspace } from '../commands/add/workspace.ts';
+import { createOrUpdateFiles } from '../commands/add/processor.ts';
+import { installPackages } from '../commands/add/utils.ts';
+
+type Addon = Adder<any>;
+export type InstallOptions<Addons extends AddonMap> = {
+	cwd: string;
+	addons: Addons;
+	options: OptionMap<Addons>;
+	packageManager: PackageManager;
+};
+
+export type AddonMap = Record<string, Addon>;
+export type OptionMap<Addons extends AddonMap> = {
+	[K in keyof Addons]: Partial<OptionValues<Addons[K]['options']>>;
+};
+
+export async function installAddon<Addons extends AddonMap>({
+	addons,
+	cwd,
+	options,
+	packageManager = 'npm'
+}: InstallOptions<Addons>): Promise<string[]> {
+	const filesToFormat = new Set<string>();
+
+	const mapped = Object.entries(addons).map(([, addon]) => addon);
+	const ordered = orderAddons(mapped);
+
+	for (const addon of ordered) {
+		const workspace = createWorkspace({ cwd, packageManager });
+		workspace.options = options[addon.id];
+
+		const files = await runAddon(workspace, addon);
+		files.forEach((f) => filesToFormat.add(f));
+	}
+
+	return Array.from(filesToFormat);
+}
+
+async function runAddon(workspace: Workspace<any>, addon: Addon): Promise<string[]> {
+	const files = new Set<string>();
+
+	// TODO: figure out why this is wrongly typed
+	// apply default adder options
+	for (const [, question] of Object.entries(addon.options as Record<string, Question>)) {
+		// we'll only apply defaults to options that don't explicitly fail their conditions
+		if (question.condition?.(workspace.options) !== false) {
+			workspace.options ??= question.default;
+		}
+	}
+
+	const pkgPath = installPackages(addon, workspace);
+	files.add(pkgPath);
+	const changedFiles = createOrUpdateFiles(addon.files, workspace);
+	changedFiles.forEach((file) => files.add(file));
+
+	for (const script of addon.scripts ?? []) {
+		if (script.condition?.(workspace) === false) continue;
+
+		try {
+			const { args, command } = resolveCommand(workspace.packageManager, 'execute', script.args)!;
+			if (workspace.packageManager === 'npm') args.unshift('--yes');
+			await exec(command, script.args, { nodeOptions: { cwd: workspace.cwd, stdio: 'pipe' } });
+		} catch (error) {
+			const typedError = error as Error;
+			throw new Error(`Failed to execute scripts '${script.description}': ` + typedError.message);
+		}
+	}
+
+	return Array.from(files);
+}
+
+// sorts them to their execution order
+function orderAddons(addons: Addon[]) {
+	return Array.from(addons).sort((a, b) => {
+		if (!a.dependsOn && !b.dependsOn) return 0;
+		if (!a.dependsOn) return -1;
+		if (!b.dependsOn) return 1;
+
+		if (a.dependsOn.includes(b.id)) return 1;
+		if (b.dependsOn.includes(a.id)) return -1;
+
+		return 0;
+	});
+}
---
+++ packages/cli/testing.ts
@@ -0,0 +1,110 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { spawn, type ChildProcessWithoutNullStreams } from 'node:child_process';
+import degit from 'degit';
+import terminate from 'terminate';
+import { create } from '@sveltejs/create';
+import { resolveCommand } from 'package-manager-detector';
+import type { PackageManager } from '@sveltejs/cli-core';
+
+export type ProjectVariant = (typeof variants)[number];
+export const variants = ['kit-js', 'kit-ts', 'vite-js', 'vite-ts'] as const;
+
+const TEMPLATES_DIR = '.templates';
+
+export type CreateProject = (options: {
+	testId: string;
+	variant: ProjectVariant;
+	/** @default true */
+	clean?: boolean;
+}) => string;
+
+type SetupOptions = {
+	cwd: string;
+	variants: readonly ProjectVariant[];
+	/** @default false */
+	clean?: boolean;
+};
+export async function setup({
+	cwd,
+	clean = false,
+	variants
+}: SetupOptions): Promise<{ templatesDir: string }> {
+	const workingDir = path.resolve(cwd);
+	if (clean && fs.existsSync(workingDir)) {
+		fs.rmSync(workingDir, { force: true, recursive: true });
+	}
+
+	// fetch the project types
+	const templatesDir = path.resolve(workingDir, TEMPLATES_DIR);
+	fs.mkdirSync(templatesDir, { recursive: true });
+	for (const variant of variants) {
+		const templatePath = path.resolve(templatesDir, variant);
+		if (fs.existsSync(templatePath)) continue;
+
+		if (variant === 'kit-js') {
+			create(templatePath, { name: variant, template: 'minimal', types: 'checkjs' });
+		} else if (variant === 'kit-ts') {
+			create(templatePath, { name: variant, template: 'minimal', types: 'typescript' });
+		} else if (variant === 'vite-js' || variant === 'vite-ts') {
+			const name = `template-svelte${variant === 'vite-ts' ? '-ts' : ''}`;
+			// TODO: should probably point this to a specific commit hash (ex: `#1234abcd`)
+			const template = degit(`vitejs/vite/packages/create-vite/${name}`, { force: true });
+			await template.clone(templatePath);
+		} else throw new Error(`Unknown project variant: ${variant}`);
+	}
+
+	return { templatesDir };
+}
+
+type CreateOptions = { cwd: string; testName: string; templatesDir: string };
+export function createProject({ cwd, testName, templatesDir }: CreateOptions): CreateProject {
+	// create the reference dir
+	const testDir = path.resolve(cwd, testName);
+	fs.mkdirSync(testDir, { recursive: true });
+	return ({ testId, variant, clean = true }) => {
+		const targetDir = path.resolve(testDir, testId);
+		if (clean && fs.existsSync(targetDir)) {
+			fs.rmSync(targetDir, { force: true, recursive: true });
+		}
+		const templatePath = path.resolve(templatesDir, variant);
+		fs.cpSync(templatePath, targetDir, { recursive: true, force: true });
+		return targetDir;
+	};
+}
+
+type PreviewOptions = { cwd: string; packageManager?: PackageManager };
+export async function startPreview({ cwd, packageManager = 'npm' }: PreviewOptions): Promise<{
+	url: string;
+	server: ChildProcessWithoutNullStreams;
+	close: () => void;
+}> {
+	const { command, args } = resolveCommand(packageManager, 'run', ['preview'])!;
+
+	const process = spawn(command, args, { stdio: 'pipe', shell: true, cwd, timeout: 120_000 });
+	const close = () => {
+		if (!process.pid) return;
+		terminate(process.pid);
+	};
+
+	return await new Promise((resolve) => {
+		process.stdout.on('data', (data: Buffer) => {
+			const value = data.toString();
+
+			// extract dev server url from console output
+			const regexUnicode = /[^\x20-\xaf]+/g;
+			const withoutUnicode = value.replace(regexUnicode, '');
+
+			const regexUnicodeDigits = /\[[0-9]{1,2}m/g;
+			const withoutColors = withoutUnicode.replace(regexUnicodeDigits, '');
+
+			const regexUrl = /http:\/\/[^:\s]+:[0-9]+\//g;
+			const urls = withoutColors.match(regexUrl);
+
+			if (urls && urls.length > 0) {
+				const url = urls[0];
+				resolve({ url, server: process, close });
+			}
+		});
+	});
+}
