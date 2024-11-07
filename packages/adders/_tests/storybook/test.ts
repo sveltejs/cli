d3ef73776911d567d18ea452a34e2b255e439d02
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-11-06 21:19:16 -0500

    simplify

--- packages/adders/_tests/storybook/test.ts
+++ packages/adders/_tests/storybook/test.ts
@@ -8,17 +8,16 @@ let port = 6006;
 test.concurrent.for(variants)('storybook loaded - %s', async (variant, { page, ...ctx }) => {
 	const cwd = await ctx.run(variant, { storybook: {} });
 
-	const p = port++;
 	const { close } = await prepareServer({
 		cwd,
 		page,
-		previewCommand: `pnpm storybook -p ${p} --ci`,
+		previewCommand: `pnpm storybook -p ${++port} --ci`,
 		buildCommand: 'echo'
 	});
 	// kill server process when we're done
 	ctx.onTestFinished(() => close());
 
-	await page.goto(`http://localhost:${p}`);
+	await page.goto(`http://localhost:${port}`);
 
 	expect(await page.$('main .sb-bar')).toBeTruthy();
 	expect(await page.$('#storybook-preview-wrapper')).toBeTruthy();
