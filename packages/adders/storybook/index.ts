8a1a47a8c979e05ae230c9234004ce2b5d341945
Manuel Serret <mserret99@gmail.com>
2024-11-10 10:57:07 +0100

    fix storybook

--- packages/adders/storybook/index.ts
+++ packages/adders/storybook/index.ts
@@ -4,7 +4,7 @@ export default defineAdder({
 	id: 'storybook',
 	homepage: 'https://storybook.js.org',
 	options: {},
-	run: ({ sv }) => {
-		sv.execute(['storybook@8.3.6', 'init', '--skip-install', '--no-dev'], 'inherit');
+	run: async ({ sv }) => {
+		await sv.execute(['storybook@8.3.6', 'init', '--skip-install', '--no-dev'], 'inherit');
 	}
 });
