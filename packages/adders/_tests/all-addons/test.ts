a2e7b839782a7cffd1af6ccf56b3f0482f1930d6
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-11-12 15:12:24 -0500

    fix test

--- packages/adders/_tests/all-addons/test.ts
+++ packages/adders/_tests/all-addons/test.ts
@@ -2,27 +2,31 @@ import process from 'node:process';
 import { expect } from '@playwright/test';
 import { setupTest } from '../_setup/suite.ts';
 import { officialAdders } from '../../index.ts';
+import type { AddonMap, OptionMap } from 'sv';
 
 const windowsCI = process.env.CI && process.platform === 'win32';
-const addons = officialAdders.reduce((addonMap, addon) => {
+const addons = officialAdders.reduce<AddonMap>((addonMap, addon) => {
 	if (addon.id === 'storybook' && windowsCI) return addonMap;
 	addonMap[addon.id] = addon;
 	return addonMap;
-}, {} as any);
+}, {});
 
-const defaultOptions = officialAdders.reduce((options, addon) => {
+const defaultOptions = officialAdders.reduce<OptionMap<typeof addons>>((options, addon) => {
 	options[addon.id] = {};
+	// TODO: we shouldn't have to apply defaults here
+	// applies defaults
 	for (const [id, question] of Object.entries(addon.options)) {
 		if (question.condition?.(options[addon.id]) !== false) {
 			options[addon.id][id] ??= question.default;
 		}
 	}
 	return options;
-}, {} as any);
+}, {});
 
-const { test, variants, prepareServer } = setupTest({ addons });
+const { test, variants, prepareServer } = setupTest(addons);
 
-test.concurrent.for(variants)('run all addons - %s', async (variant, { page, ...ctx }) => {
+const kitOnly = variants.filter((v) => v.startsWith('kit'));
+test.concurrent.for(kitOnly)('run all addons - %s', async (variant, { page, ...ctx }) => {
 	const cwd = await ctx.run(variant, defaultOptions);
 
 	const { close } = await prepareServer({ cwd, page });
