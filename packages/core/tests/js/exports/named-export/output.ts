18e937da88965adfbc0d20e129bfe4a16647bc0b
Manuel Serret <mserret99@gmail.com>
2024-12-29 10:30:09 +0100

    update snapshots

--- packages/core/tests/js/exports/named-export/output.ts
+++ packages/core/tests/js/exports/named-export/output.ts
@@ -1,7 +1,2 @@
-export const variable = {
-	test: 'string'
-};
-
-export const variable2 = {
-	test2: 'string2'
-};
+export const variable = { test: 'string' };
+export const variable2 = { test2: 'string2' };
