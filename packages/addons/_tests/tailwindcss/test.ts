7fc1a979ae83df2f38b94abe25dfc4592c62dea0
jycouet <jycouet@gmail.com>
2025-01-23 23:46:37 +0100

    correct oklch (tmp only tailwind)

--- packages/addons/_tests/tailwindcss/test.ts
+++ packages/addons/_tests/tailwindcss/test.ts
@@ -17,7 +17,7 @@ test.concurrent.for(variants)('none - %s', async (variant, { page, ...ctx }) => 
 
 	const el = page.getByTestId('base');
 	await expect(el).toHaveCSS('background-color', 'oklch(0.446 0.043 257.281)');
-	await expect(el).toHaveCSS('border-color', 'oklch(0.984 0.003 247.858)');
+	await expect(el).toHaveCSS('border-color', 'oklch(0.985 0.002 247.839)');
 	await expect(el).toHaveCSS('border-width', '4px');
 	await expect(el).toHaveCSS('margin-top', '4px');
 });
