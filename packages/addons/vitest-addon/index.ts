2cb6f97044d9cfcdf38bc177de42d0178ca4a596
Manuel <mserret99@gmail.com>
2025-02-07 14:30:47 +0100

    remove if

--- packages/addons/vitest-addon/index.ts
+++ packages/addons/vitest-addon/index.ts
@@ -88,20 +88,18 @@ export default defineAddon({
 					import { vi } from 'vitest';
 
 					// required for svelte5 + jsdom as jsdom does not support matchMedia
-					if(typeof window != undefined && !window.matchMedia) {
-						Object.defineProperty(window, 'matchMedia', {
-							writable: true,
-							enumerable: true,
-							value: vi.fn().mockImplementation(query => ({
-								matches: false,
-								media: query,
-								onchange: null,
-								addEventListener: vi.fn(),
-								removeEventListener: vi.fn(),
-								dispatchEvent: vi.fn(),
-							})),
-						})
-					}
+					Object.defineProperty(window, 'matchMedia', {
+						writable: true,
+						enumerable: true,
+						value: vi.fn().mockImplementation(query => ({
+							matches: false,
+							media: query,
+							onchange: null,
+							addEventListener: vi.fn(),
+							removeEventListener: vi.fn(),
+							dispatchEvent: vi.fn(),
+						})),
+					})
 
 					// add more mocks here if you need them
 				`;
