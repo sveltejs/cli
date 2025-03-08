31f26f29becfcace3f03870667fe461f1fff4603
Hugos68 <hugokorteapple@gmail.com>
2025-03-08 20:39:19 +0100

    Add `unshift` to js tooling

--- packages/core/tooling/js/array.ts
+++ packages/core/tooling/js/array.ts
@@ -40,3 +40,36 @@ export function push(
 		}
 	}
 }
+
+export function unshift(
+	ast: AstTypes.ArrayExpression,
+	data: string | AstKinds.ExpressionKind | AstKinds.SpreadElementKind
+): void {
+	if (typeof data === 'string') {
+		const existingLiterals = ast.elements.filter(
+			(x): x is AstTypes.StringLiteral => x?.type == 'StringLiteral'
+		);
+		let literal = existingLiterals.find((x) => x.value == data);
+
+		if (!literal) {
+			literal = {
+				type: 'StringLiteral',
+				value: data
+			};
+			ast.elements.unshift(literal);
+		}
+	} else {
+		let anyNodeEquals = false;
+		const elements = ast.elements as AstTypes.ASTNode[];
+		for (const node of elements) {
+			if (areNodesEqual(data, node)) {
+				anyNodeEquals = true;
+			}
+		}
+
+		if (!anyNodeEquals) {
+			ast.elements.unshift(data);
+		}
+	}
+
+}
