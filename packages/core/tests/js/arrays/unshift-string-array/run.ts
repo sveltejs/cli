d801b0e459f10330549494bcc081a2bcbb514534
Hugo Korte <63101006+Hugos68@users.noreply.github.com>
2025-03-09 07:06:28 +0100

    fix: insert the `tailwindcss` vite plugin at the start of the plugin array (#478)
    * Fix plugin order
    
    * changeset
    
    * Add `unshift` to js tooling
    
    * Add test cases for `unshift`
    
    * format
    
    * tweaks
    
    * oops, fix
    
    ---------
    
    Co-authored-by: AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>

---
+++ packages/core/tests/js/arrays/unshift-string-array/run.ts
@@ -0,0 +1,13 @@
+import { array, variables } from '@sveltejs/cli-core/js';
+import type { ScriptFileEditor } from '@sveltejs/cli-core';
+
+export function run({ ast }: ScriptFileEditor<any>): void {
+	const array1 = array.createEmpty();
+	array.unshift(array1, 'test');
+	array.unshift(array1, 'test2');
+	array.unshift(array1, 'test'); // make sure items are not duplicated
+
+	// create declaration so that we serialize everything
+	const declaration = variables.declaration(ast, 'const', 'array', array1);
+	ast.body.push(declaration);
+}
