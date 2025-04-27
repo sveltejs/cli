5fbfc69fd91742a8a002af54ae73b1721be95f18
Manuel Serret <mserret99@gmail.com>
2025-04-27 08:23:43 +0200

    add debug stuff

--- packages/cli/bin.ts
+++ packages/cli/bin.ts
@@ -1,13 +1,10 @@
 #!/usr/bin/env node
 
-import pkg from './package.json' with { type: 'json' };
-import { program } from 'commander';
-import { add } from './commands/add/index.ts';
-import { create } from './commands/create.ts';
-import { migrate } from './commands/migrate.ts';
-import { check } from './commands/check.ts';
-import { helpConfig } from './utils/common.ts';
+import process from 'node:process';
 
-program.name(pkg.name).version(pkg.version, '-v, --version').configureHelp(helpConfig);
-program.addCommand(create).addCommand(add).addCommand(migrate).addCommand(check);
-program.parse();
+const paraglide = process.argv[3];
+console.log('received paraglide args: ' + paraglide);
+
+const expectedResult = 'paraglide=languageTags:en,de,demo:no';
+if (paraglide !== expectedResult) console.log('result unexpected, expected: ' + expectedResult);
+else console.log('ok');
