b75c49e9258284cad3a51e9aad2680ea7dd46670
Rich Harris <rich.harris@vercel.com>
2025-06-20 15:56:09 -0400

    dont select devtools-json by default

--- packages/core/addon/config.ts
+++ packages/core/addon/config.ts
@@ -38,7 +38,6 @@ export type Addon<Args extends OptionDefinition> = {
 			dependsOn: (name: string) => void;
 			unsupported: (reason: string) => void;
 			runsAfter: (addonName: string) => void;
-			defaultSelection: (args: { create: boolean; add: boolean }) => void;
 		}
 	) => MaybePromise<void>;
 	run: (workspace: Workspace<Args> & { sv: SvApi }) => MaybePromise<void>;
@@ -61,12 +60,7 @@ export function defineAddon<Args extends OptionDefinition>(config: Addon<Args>):
 	return config;
 }
 
-export type AddonSetupResult = {
-	dependsOn: string[];
-	unsupported: string[];
-	runsAfter: string[];
-	defaultSelection: { create: boolean; add: boolean };
-};
+export type AddonSetupResult = { dependsOn: string[]; unsupported: string[]; runsAfter: string[] };
 
 export type AddonWithoutExplicitArgs = Addon<Record<string, Question>>;
 export type AddonConfigWithoutExplicitArgs = Addon<Record<string, Question>>;
