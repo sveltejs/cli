69f9538081c7ed85d68d3d574fef12a15255b3a0
jycouet <jycouet@gmail.com>
2025-06-29 21:06:10 +0200

    feat(add): Add minimum Node.js version warning if below 16.

--- packages/cli/utils/common.ts
+++ packages/cli/utils/common.ts
@@ -4,6 +4,7 @@ import * as p from '@clack/prompts';
 import type { Argument, HelpConfiguration, Option } from 'commander';
 import { UnsupportedError } from './errors.ts';
 import process from 'node:process';
+import semver from 'semver';
 
 const NO_PREFIX = '--no-';
 let options: readonly Option[] = [];
@@ -75,6 +76,14 @@ type MaybePromise = () => Promise<void> | void;
 export async function runCommand(action: MaybePromise): Promise<void> {
 	try {
 		p.intro(`Welcome to the Svelte CLI! ${pc.gray(`(v${pkg.version})`)}`);
+
+		const isUnSupportedNodeVersion =
+			process.versions.node && semver.lt(process.versions.node, '16.0.0');
+		if (isUnSupportedNodeVersion) {
+			p.log.warn(
+				`You are using Node.js ${pc.red(process.versions.node)}, please upgrade to Node.js 16 or higher.`
+			);
+		}
 		await action();
 		p.outro("You're all set!");
 	} catch (e) {
