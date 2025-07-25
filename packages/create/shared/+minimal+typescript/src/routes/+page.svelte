8357be3b066d6e009819e5a77c67629478ee449c
jycouet <jycouet@gmail.com>
2025-07-25 23:33:28 +0200

    with or without ts

---
+++ packages/create/shared/+minimal+typescript/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
