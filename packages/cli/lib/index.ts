b97f76c828a9f3f92216511804da1bcccf6aed91
jycouet <jycouet@gmail.com>
2025-09-19 12:00:36 +0200

    export TemplateType and LanguageType

--- packages/cli/lib/index.ts
+++ packages/cli/lib/index.ts
@@ -1,4 +1,4 @@
-export { create } from '@sveltejs/create';
+export { create, type TemplateType, type LanguageType } from '@sveltejs/create';
 export { installAddon } from './install.ts';
 export type { AddonMap, InstallOptions, OptionMap } from './install.ts';
 export { officialAddons } from '@sveltejs/addons';
