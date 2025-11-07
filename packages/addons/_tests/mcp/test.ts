20c9dd311f1ce3966fd6f2a2f0937f853031a18b
jycouet <jycouet@gmail.com>
2025-11-07 18:06:12 +0100

    having a snapshot of all versions

--- packages/addons/_tests/mcp/test.ts
+++ packages/addons/_tests/mcp/test.ts
@@ -1,4 +1,4 @@
-import { expect } from '@playwright/test';
+import { expect } from 'vitest';
 import { setupTest } from '../_setup/suite.ts';
 import mcp from '../../mcp/index.ts';
 import fs from 'node:fs';
@@ -9,9 +9,15 @@ const { test, testCases } = setupTest(
 	{
 		kinds: [
 			{
-				type: 'default',
+				type: 'default-local',
 				options: {
 					mcp: { ide: ['claude-code', 'cursor', 'gemini', 'opencode', 'vscode'], setup: 'local' }
+				}
+			},
+			{
+				type: 'default-remote',
+				options: {
+					mcp: { ide: ['claude-code', 'cursor', 'gemini', 'opencode', 'vscode'], setup: 'remote' }
 				}
 			}
 		],
@@ -39,18 +45,175 @@ const { test, testCases } = setupTest(
 	}
 );
 
-test.concurrent.for(testCases)('mcp $variant', (testCase, ctx) => {
+test.concurrent.for(testCases)('mcp $kind.type $variant', (testCase, ctx) => {
 	const cwd = ctx.cwd(testCase);
 
-	const cursorPath = path.resolve(cwd, `.cursor/mcp.json`);
-	const cursorMcpContent = fs.readFileSync(cursorPath, 'utf8');
+	const getContent = (filePath: string) => {
+		const cursorPath = path.resolve(cwd, filePath);
+		return fs.readFileSync(cursorPath, 'utf8');
+	};
+
+	const cursorMcpContent = getContent(`.cursor/mcp.json`);
 
 	// should keep other MCPs
 	expect(cursorMcpContent).toContain(`anotherMCP`);
-	// should have the svelte level
-	expect(cursorMcpContent).toContain(`svelte`);
-	// should have local conf
-	expect(cursorMcpContent).toContain(`@sveltejs/mcp`);
 	// should remove old svelte config
 	expect(cursorMcpContent).not.toContain(`thing`);
+
+	const fullConf: Record<string, any> = {};
+	const ides = {
+		'claude-code': '.mcp.json',
+		cursor: '.cursor/mcp.json',
+		gemini: '.gemini/settings.json',
+		opencode: 'opencode.json',
+		vscode: '.vscode/mcp.json'
+	} as const;
+
+	for (const [ide, filePath] of Object.entries(ides)) {
+		fullConf[ide] = {
+			filePath,
+			content: JSON.parse(getContent(filePath))
+		};
+	}
+
+	if (testCase.kind.type === 'default-local') {
+		expect(fullConf).toMatchInlineSnapshot(`
+			{
+			  "claude-code": {
+			    "content": {
+			      "mcpServers": {
+			        "svelte": {
+			          "args": [
+			            "-y",
+			            "@sveltejs/mcp",
+			          ],
+			          "command": "npx",
+			          "env": {},
+			          "type": "stdio",
+			        },
+			      },
+			    },
+			    "filePath": ".mcp.json",
+			  },
+			  "cursor": {
+			    "content": {
+			      "mcpServers": {
+			        "anotherMCP": {},
+			        "svelte": {
+			          "args": [
+			            "-y",
+			            "@sveltejs/mcp",
+			          ],
+			          "command": "npx",
+			        },
+			      },
+			    },
+			    "filePath": ".cursor/mcp.json",
+			  },
+			  "gemini": {
+			    "content": {
+			      "mcpServers": {
+			        "svelte": {
+			          "args": [
+			            "-y",
+			            "@sveltejs/mcp",
+			          ],
+			          "command": "npx",
+			        },
+			      },
+			    },
+			    "filePath": ".gemini/settings.json",
+			  },
+			  "opencode": {
+			    "content": {
+			      "$schema": "https://opencode.ai/config.json",
+			      "mcp": {
+			        "svelte": {
+			          "command": [
+			            "npx",
+			            "-y",
+			            "@sveltejs/mcp",
+			          ],
+			          "type": "local",
+			        },
+			      },
+			    },
+			    "filePath": "opencode.json",
+			  },
+			  "vscode": {
+			    "content": {
+			      "servers": {
+			        "svelte": {
+			          "args": [
+			            "-y",
+			            "@sveltejs/mcp",
+			          ],
+			          "command": "npx",
+			        },
+			      },
+			    },
+			    "filePath": ".vscode/mcp.json",
+			  },
+			}
+		`);
+	} else if (testCase.kind.type === 'default-remote') {
+		expect(fullConf).toMatchInlineSnapshot(`
+			{
+			  "claude-code": {
+			    "content": {
+			      "mcpServers": {
+			        "svelte": {
+			          "type": "http",
+			          "url": "https://mcp.svelte.dev/mcp",
+			        },
+			      },
+			    },
+			    "filePath": ".mcp.json",
+			  },
+			  "cursor": {
+			    "content": {
+			      "mcpServers": {
+			        "anotherMCP": {},
+			        "svelte": {
+			          "url": "https://mcp.svelte.dev/mcp",
+			        },
+			      },
+			    },
+			    "filePath": ".cursor/mcp.json",
+			  },
+			  "gemini": {
+			    "content": {
+			      "mcpServers": {
+			        "svelte": {
+			          "url": "https://mcp.svelte.dev/mcp",
+			        },
+			      },
+			    },
+			    "filePath": ".gemini/settings.json",
+			  },
+			  "opencode": {
+			    "content": {
+			      "$schema": "https://opencode.ai/config.json",
+			      "mcp": {
+			        "svelte": {
+			          "type": "remote",
+			          "url": "https://mcp.svelte.dev/mcp",
+			        },
+			      },
+			    },
+			    "filePath": "opencode.json",
+			  },
+			  "vscode": {
+			    "content": {
+			      "servers": {
+			        "svelte": {
+			          "url": "https://mcp.svelte.dev/mcp",
+			        },
+			      },
+			    },
+			    "filePath": ".vscode/mcp.json",
+			  },
+			}
+		`);
+	}
 });
