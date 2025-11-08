7227e16c429045a9510881d7513f4eedce3b0614
jycouet <jycouet@gmail.com>
2025-11-08 21:09:07 +0100

    fixing mcp issue

--- packages/addons/mcp/index.ts
+++ packages/addons/mcp/index.ts
@@ -101,6 +101,8 @@ export default defineAddon({
 
 		for (const ide of options.ide) {
 			const value = configurator[ide];
+
+			if (value === undefined) continue;
 			if ('other' in value) continue;
 
 			const { mcpServersKey, filePath, typeLocal, typeRemote, env, schema, command, args } = value;
