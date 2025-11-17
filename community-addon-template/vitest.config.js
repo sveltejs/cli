1397e8b5febe1b3dfce41b0ac3e05e3c86aec494
jycouet <jycouet@gmail.com>
2025-11-17 10:43:32 +0100

    back to 3min

--- community-addon-template/vitest.config.js
+++ community-addon-template/vitest.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
 	test: {
 		include: ['tests/**/*.test.{js,ts}'],
 		exclude: ['tests/setup/*'],
-		testTimeout: ONE_MINUTE * 4,
-		hookTimeout: ONE_MINUTE * 4,
+		testTimeout: ONE_MINUTE * 3,
+		hookTimeout: ONE_MINUTE * 3,
 		globalSetup: ['tests/setup/global.ts'],
 		expect: {
 			requireAssertions: true
