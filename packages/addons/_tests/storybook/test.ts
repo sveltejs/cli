059d0cd2b4d6cedc25efdb036deb8a05cdd54b40
jycouet <jycouet@gmail.com>
2025-11-18 19:13:59 +0100

    prefetch storybook ONCE in the global setup. (for all-addons + storybook + others...)

--- packages/addons/_tests/storybook/test.ts
+++ packages/addons/_tests/storybook/test.ts
@@ -1,9 +1,7 @@
 import process from 'node:process';
-import { exec } from 'tinyexec';
 import { expect } from '@playwright/test';
-import { beforeAll } from 'vitest';
 import { setupTest } from '../_setup/suite.ts';
-import storybook, { STORYBOOK_VERSION } from '../../storybook/index.ts';
+import storybook from '../../storybook/index.ts';
 import eslint from '../../eslint/index.ts';
 
 // we're including the `eslint` add-on to prevent `storybook` from modifying this repo's `eslint.config.js`
@@ -14,13 +12,6 @@ const { test, testCases, prepareServer } = setupTest(
 
 let port = 6006;
 const CI = Boolean(process.env.CI);
-
-beforeAll(async () => {
-	if (CI) {
-		// prefetch the storybook cli during ci to reduce fetching errors in tests
-		await exec('pnpm', ['dlx', `create-storybook@${STORYBOOK_VERSION}`, '--version']);
-	}
-});
 
 test.for(testCases)(
 	'storybook $variant',
