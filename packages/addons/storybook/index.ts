c0d9e99dbf7172f58b22475abafe12a26137b792
Manuel <30698007+manuel3108@users.noreply.github.com>
2025-11-28 15:49:08 +0100

    chore: storybook new cli ux (#781)
    * chore: storybook new cli ux
    
    * Update packages/addons/storybook/index.ts
    
    Co-authored-by: Jeppe Reinhold <jeppe@reinhold.is>
    
    * link storybook versions
    
    * with this syntax!
    
    * --features docs
    
    * prefetch storybook ONCE in the global setup. (for all-addons + storybook + others...)
    
    * let's see if now in // t's possible in CI
    
    * --no-features
    
    * storybook 10.1.0
    
    ---------
    
    Co-authored-by: jyc.dev <jycouet@gmail.com>
    Co-authored-by: Jeppe Reinhold <jeppe@reinhold.is>

--- packages/addons/storybook/index.ts
+++ packages/addons/storybook/index.ts
@@ -2,6 +2,8 @@ import process from 'node:process';
 import { defineAddon } from '@sveltejs/cli-core';
 import { getNodeTypesVersion } from '../common.ts';
 
+export const STORYBOOK_VERSION = '10.1.0';
+
 export default defineAddon({
 	id: 'storybook',
 	shortDescription: 'frontend workshop',
@@ -12,7 +14,12 @@ export default defineAddon({
 		runsAfter('eslint');
 	},
 	run: async ({ sv }) => {
-		const args = ['create-storybook@latest', '--skip-install', '--no-dev'];
+		const args = [
+			`create-storybook@${STORYBOOK_VERSION}`,
+			'--skip-install',
+			'--no-dev',
+			'--no-features'
+		];
 
 		// skips the onboarding prompt during tests
 		if (process.env.NODE_ENV?.toLowerCase() === 'test') args.push('--yes');
