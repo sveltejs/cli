9906c8990e2dab2b6c132b56aa9e46c3b12f5f52
jycouet <jycouet@gmail.com>
2025-11-30 21:31:04 +0100

    add even storybook!

--- packages/cli/tests/cli.ts
+++ packages/cli/tests/cli.ts
@@ -39,54 +39,74 @@ describe('cli', () => {
 		}
 	];
 
-	it.for(testCases)('should create a new project with name $projectName', async (testCase) => {
-		const { projectName, args } = testCase;
-		const svBinPath = path.resolve(monoRepoPath, 'packages', 'cli', 'dist', 'bin.js');
-		const testOutputPath = path.resolve(monoRepoPath, '.test-output', 'cli', projectName);
+	it.for(testCases)(
+		'should create a new project with name $projectName',
+		{ timeout: 10_000 },
+		async (testCase) => {
+			const { projectName, args } = testCase;
+			const svBinPath = path.resolve(monoRepoPath, 'packages', 'cli', 'dist', 'bin.js');
+			const testOutputPath = path.resolve(monoRepoPath, '.test-output', 'cli', projectName);
 
-		const result = await exec(
-			'node',
-			[
-				svBinPath,
-				'create',
-				testOutputPath,
-				'--template',
-				'minimal',
-				'--types',
-				'ts',
-				'--no-install',
-				...args
-			],
-			{ nodeOptions: { stdio: 'pipe' } }
-		);
-
-		// cli finished well
-		expect(result.exitCode).toBe(0);
-
-		// test output path exists
-		expect(fs.existsSync(testOutputPath)).toBe(true);
-
-		// package.json has a name
-		const packageJsonPath = path.resolve(testOutputPath, 'package.json');
-		const packageJson = parseJson(fs.readFileSync(packageJsonPath, 'utf-8'));
-		expect(packageJson.name).toBe(projectName);
-
-		if (projectName === 'create-with-all-addons') {
-			const snapPath = path.resolve(
-				monoRepoPath,
-				'packages',
-				'cli',
-				'tests',
-				'create-with-all-addons'
+			const result = await exec(
+				'node',
+				[
+					svBinPath,
+					'create',
+					testOutputPath,
+					'--template',
+					'minimal',
+					'--types',
+					'ts',
+					'--no-install',
+					...args
+				],
+				{ nodeOptions: { stdio: 'pipe' } }
 			);
-			const relativeFiles = fs.readdirSync(snapPath, { recursive: true }) as string[];
 
-			for (const relativeFile of relativeFiles) {
-				if (!fs.statSync(path.resolve(snapPath, relativeFile)).isFile()) continue;
-				const generated = fs.readFileSync(path.resolve(testOutputPath, relativeFile), 'utf-8');
-				const snap = fs.readFileSync(path.resolve(snapPath, relativeFile), 'utf-8');
-				expect(generated, `file "${relativeFile}" does not match snapshot`).toBe(snap);
+			// cli finished well
+			expect(result.exitCode).toBe(0);
+
+			// test output path exists
+			expect(fs.existsSync(testOutputPath)).toBe(true);
+
+			// package.json has a name
+			const packageJsonPath = path.resolve(testOutputPath, 'package.json');
+			const packageJson = parseJson(fs.readFileSync(packageJsonPath, 'utf-8'));
+			expect(packageJson.name).toBe(projectName);
+
+			if (projectName === 'create-with-all-addons') {
+				const snapPath = path.resolve(
+					monoRepoPath,
+					'packages',
+					'cli',
+					'tests',
+					'create-with-all-addons'
+				);
+
+				// How to update this snapshot?
+				// rm -rf packages/cli/tests/create-with-all-addons
+				// trigger tests again... and check the diff!
+				if (!fs.existsSync(snapPath)) {
+					// let's copy the generated files to be the snapshots!
+					fs.cpSync(testOutputPath, snapPath, {
+						recursive: true,
+						filter(source) {
+							if (source.includes('node_modules')) return false;
+							const doNotCopyEnding = ['.png', 'avif', '.svg'];
+							if (doNotCopyEnding.some((ending) => source.endsWith(ending))) return false;
+							return true;
+						}
+					});
+				}
+				const relativeFiles = fs.readdirSync(snapPath, { recursive: true }) as string[];
+
+				for (const relativeFile of relativeFiles) {
+					if (!fs.statSync(path.resolve(snapPath, relativeFile)).isFile()) continue;
+					const generated = fs.readFileSync(path.resolve(testOutputPath, relativeFile), 'utf-8');
+					const snap = fs.readFileSync(path.resolve(snapPath, relativeFile), 'utf-8');
+					expect(generated, `file "${relativeFile}" does not match snapshot`).toBe(snap);
+				}
 			}
 		}
-	});
+	);
 });
--- packages/cli/tests/create-with-all-addons/eslint.config.js
+++ packages/cli/tests/create-with-all-addons/eslint.config.js
@@ -1,4 +1,11 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import prettier from 'eslint-config-prettier';
 import svelte from 'eslint-plugin-svelte';
 
-export default [prettier, ...svelte.configs.prettier];
+export default [
+  prettier,
+  ...svelte.configs.prettier,
+  ...storybook.configs["flat/recommended"]
+];
