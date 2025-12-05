d86ec9739db932a00d4063604c90f5812c0b0921
jycouet <jycouet@gmail.com>
2025-12-05 15:44:16 +0100

    keep replaceAll for script ending

--- packages/cli/lib/install.ts
+++ packages/cli/lib/install.ts
@@ -155,7 +155,8 @@ async function runAddon({ addon, multiple, workspace, workspaceOptions }: RunAdd
 				fileContent = content(fileContent);
 				if (!fileContent) return fileContent;
 
-				writeFile(workspace, path, fileContent);
+				// TODO: fix https://github.com/rolldown/tsdown/issues/575 to remove the `replaceAll`
+				writeFile(workspace, path, fileContent.replaceAll('<\\/script>', '</script>'));
 				files.add(path);
 			} catch (e) {
 				if (e instanceof Error) {
