76421d7606677e0d2b21d18ebe715c28b9cebd3c
jycouet <jycouet@gmail.com>
2025-12-08 08:42:33 +0100

    hello addon tests update to have browser test as option

--- packages/sv/lib/create/templates/addon/tests/addon.test.ts
+++ packages/sv/lib/create/templates/addon/tests/addon.test.ts
@@ -1,29 +1,43 @@
 import { expect } from '@playwright/test';
-import { setupTest } from './setup/suite.ts';
+import { setupTest } from './setup/suite.js';
 import addon from '../src/index.js';
+import path from 'node:path';
+import fs from 'node:fs';
 
 const id = addon.id;
+
+// set to true to enable browser testing
+const browser = false;
+
 const { test, prepareServer, testCases } = setupTest(
 	{ addon },
 	{
 		kinds: [{ type: 'default', options: { [id]: addon } }],
-		filter: (testCase) => testCase.variant.includes('kit')
+		filter: (testCase) => testCase.variant.includes('kit'),
+		browser
 	}
 );
 
-test.concurrent.for(testCases)(
-	'~SV-NAME-TODO~ $kind.type $variant',
-	async (testCase, { page, ...ctx }) => {
-		const cwd = ctx.cwd(testCase);
+test.concurrent.for(testCases)('hello $kind.type $variant', async (testCase, { page, ...ctx }) => {
+	const cwd = ctx.cwd(testCase);
 
+	const msg =
+		"This is a text file made by the Community Addon Template demo for the add-on: 'hello'!";
+
+	const contentPath = path.resolve(cwd, `src/lib/hello/content.txt`);
+	const contentContent = fs.readFileSync(contentPath, 'utf8');
+
+	// Check if we have the imports
+	expect(contentContent).toContain(msg);
+
+	// For browser testing
+	if (browser) {
 		const { close } = await prepareServer({ cwd, page });
 		// kill server process when we're done
 		ctx.onTestFinished(async () => await close());
 
 		// expectations
 		const textContent = await page.locator('p').last().textContent();
-		const msg =
-			"This is a text file made by the Community Addon Template demo for the add-on: '~SV-NAME-TODO~'!";
 		if (testCase.variant.includes('kit')) {
 			expect(textContent).toContain(msg);
 		} else {
@@ -31,4 +45,4 @@ test.concurrent.for(testCases)(
 			expect(textContent).not.toContain(msg);
 		}
 	}
-);
+});
