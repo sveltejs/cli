ff827560f64ab78a004c4294b04455ad40f93080
jycouet <jycouet@gmail.com>
2025-12-11 20:59:03 +0100

    add failing tests

--- packages/sv/lib/cli/tests/snapshots/create-with-all-addons/src/routes/+layout.svelte
+++ packages/sv/lib/cli/tests/snapshots/create-with-all-addons/src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { page } from '$app/state';
+	import { locales, localizeHref } from '$lib/paraglide/runtime';
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 
@@ -6,4 +8,14 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
 {@render children()}
+<div style="display:none">
+	{#each locales as locale}
+		<a
+			href={localizeHref(page.url.pathname, { locale })}
+		>
+			{locale}
+		</a>
+	{/each}
+</div>
