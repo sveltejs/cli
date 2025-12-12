799c92b93c23841ecc2d398e8515cd26fe658af6
jycouet <jycouet@gmail.com>
2025-12-12 21:59:00 +0100

    fixing tests

--- packages/sv/lib/cli/tests/addon.ts
+++ packages/sv/lib/cli/tests/addon.ts
@@ -5,28 +5,28 @@ import { sanitizeAddons } from '../add/index.ts';
 describe('sanitizeAddons', () => {
 	it('should find official addon', () => {
 		expect(sanitizeAddons([{ id: 'eslint' }])).toEqual([
-			{ id: 'eslint', options: [], kind: 'official' }
+			{ id: 'eslint', options: [], kind: 'official', resolvedId: 'eslint' }
 		]);
 	});
 	it('should find official addon with alias', () => {
 		expect(sanitizeAddons([{ id: 'tailwind' }])).toEqual([
-			{ id: 'tailwindcss', options: [], kind: 'official' }
+			{ id: 'tailwindcss', options: [], kind: 'official', resolvedId: 'tailwindcss' }
 		]);
 	});
 	it('should have 2', () => {
 		expect(sanitizeAddons([{ id: 'eslint' }, { id: 'tailwindcss' }])).toEqual([
-			{ id: 'eslint', options: [], kind: 'official' },
-			{ id: 'tailwindcss', options: [], kind: 'official' }
+			{ id: 'eslint', options: [], kind: 'official', resolvedId: 'eslint' },
+			{ id: 'tailwindcss', options: [], kind: 'official', resolvedId: 'tailwindcss' }
 		]);
 	});
 	it('should dedupe even with alias', () => {
 		expect(sanitizeAddons([{ id: 'tailwind' }, { id: 'tailwindcss' }])).toEqual([
-			{ id: 'tailwindcss', options: [], kind: 'official' }
+			{ id: 'tailwindcss', options: [], kind: 'official', resolvedId: 'tailwindcss' }
 		]);
 	});
 	it('should find file addons', () => {
 		expect(sanitizeAddons([{ id: 'file:../' }])).toEqual([
-			{ id: 'file:../', options: [], kind: 'file' }
+			{ id: 'file:../', options: [], kind: 'file', resolvedId: '../' }
 		]);
 	});
 });
