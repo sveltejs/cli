5c117bde0025abe47e15932b9783b487677d2399
jycouet <jycouet@gmail.com>
2025-12-12 18:39:12 +0100

    update default location

--- packages/sv/lib/cli/tests/snapshots/@my-org/sv/tests/setup/global.js
+++ packages/sv/lib/cli/tests/snapshots/@my-org/sv/tests/setup/global.js
@@ -1,7 +1,7 @@
 import { fileURLToPath } from 'node:url';
 import { setupGlobal } from 'sv/testing';
 
-const TEST_DIR = fileURLToPath(new URL('./.test-output/', import.meta.url));
+const TEST_DIR = fileURLToPath(new URL('../../.test-output/', import.meta.url));
 
 export default setupGlobal({
 	TEST_DIR,
