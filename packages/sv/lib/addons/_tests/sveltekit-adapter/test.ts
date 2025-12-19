7b3271bff61faac7ee2b4d2d3077eaf31ccd09a5
jycouet <jycouet@gmail.com>
2025-12-19 18:02:57 +0100

    adding a test with cloudflare

--- packages/sv/lib/addons/_tests/sveltekit-adapter/test.ts
+++ packages/sv/lib/addons/_tests/sveltekit-adapter/test.ts
@@ -5,36 +5,42 @@ import sveltekitAdapter from '../../sveltekit-adapter/index.ts';
 import { setupTest } from '../_setup/suite.ts';
 
 const addonId = sveltekitAdapter.id;
-const { test, testCases, prepareServer } = setupTest(
+const { test, testCases } = setupTest(
 	{ [addonId]: sveltekitAdapter },
 	{
 		kinds: [
 			{ type: 'node', options: { [addonId]: { adapter: 'node' } } },
-			{ type: 'auto', options: { [addonId]: { adapter: 'auto' } } }
+			{ type: 'auto', options: { [addonId]: { adapter: 'auto' } } },
+			{
+				type: 'cloudflare-workers',
+				options: { [addonId]: { adapter: 'cloudflare', cfTarget: 'workers' } }
+			},
+			{
+				type: 'cloudflare-pages',
+				options: { [addonId]: { adapter: 'cloudflare', cfTarget: 'pages' } }
+			}
 		],
-		filter: (addonTestCase) => addonTestCase.variant.includes('kit')
+		filter: (addonTestCase) => addonTestCase.variant.includes('kit'),
+		browser: false
 	}
 );
 
-test.concurrent.for(testCases)(
-	'adapter $kind.type $variant',
-	async (testCase, { page, ...ctx }) => {
-		const cwd = ctx.cwd(testCase);
+test.concurrent.for(testCases)('adapter $kind.type $variant', async (testCase, { ...ctx }) => {
+	const cwd = ctx.cwd(testCase);
 
-		const { close } = await prepareServer({ cwd, page });
-		// kill server process when we're done
-		ctx.onTestFinished(async () => await close());
-
-		if (testCase.kind.type === 'node') {
-			expect(await readFile(join(cwd, 'svelte.config.js'), 'utf8')).not.toMatch('adapter-auto');
-			expect(await readFile(join(cwd, 'svelte.config.js'), 'utf8')).not.toMatch(
-				'adapter-auto only supports some environments'
-			);
-		} else if (testCase.kind.type === 'auto') {
-			expect(await readFile(join(cwd, 'svelte.config.js'), 'utf8')).toMatch('adapter-auto');
-			expect(await readFile(join(cwd, 'svelte.config.js'), 'utf8')).toMatch(
-				'adapter-auto only supports some environments'
-			);
-		}
+	if (testCase.kind.type === 'node') {
+		expect(await readFile(join(cwd, 'svelte.config.js'), 'utf8')).not.toMatch('adapter-auto');
+		expect(await readFile(join(cwd, 'svelte.config.js'), 'utf8')).not.toMatch(
+			'adapter-auto only supports some environments'
+		);
+	} else if (testCase.kind.type === 'auto') {
+		expect(await readFile(join(cwd, 'svelte.config.js'), 'utf8')).toMatch('adapter-auto');
+		expect(await readFile(join(cwd, 'svelte.config.js'), 'utf8')).toMatch(
+			'adapter-auto only supports some environments'
+		);
+	} else if (testCase.kind.type === 'cloudflare-workers') {
+		expect(await readFile(join(cwd, 'wrangler.jsonc'), 'utf8')).toMatch('ASSETS');
+	} else if (testCase.kind.type === 'cloudflare-pages') {
+		expect(await readFile(join(cwd, 'wrangler.jsonc'), 'utf8')).toMatch('pages_build_output_dir');
 	}
-);
+});
