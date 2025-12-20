0c291248045e1243cb43f34bbd82ad0b57ffb1de
jycouet <jycouet@gmail.com>
2025-12-20 13:24:56 +0100

    sniff

--- packages/sv/lib/addons/_tests/sveltekit-adapter/test.ts
+++ packages/sv/lib/addons/_tests/sveltekit-adapter/test.ts
@@ -1,5 +1,5 @@
 import { join } from 'node:path';
-import { readFileSync } from 'node:fs/promises';
+import { readFileSync } from 'node:fs';
 import { expect } from '@playwright/test';
 import sveltekitAdapter from '../../sveltekit-adapter/index.ts';
 import { setupTest } from '../_setup/suite.ts';
