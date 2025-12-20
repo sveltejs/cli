79078d53165460c9a730dd1ea0c2d0ce2a0e1308
jycouet <jycouet@gmail.com>
2025-12-20 13:47:13 +0100

    paraglide need browser testing!

--- packages/sv/lib/addons/_tests/paraglide/test.ts
+++ packages/sv/lib/addons/_tests/paraglide/test.ts
@@ -6,19 +6,22 @@ import path from 'node:path';
 
 const langs = ['en', 'fr', 'hu'];
 
-const { test, testCases } = setupTest(
+const { test, testCases, prepareServer } = setupTest(
 	{ paraglide },
 	{
 		kinds: [
 			{ type: 'default', options: { paraglide: { demo: true, languageTags: langs.join(',') } } }
 		],
-		filter: (addonTestCase) => addonTestCase.variant.includes('kit'),
-		browser: false
+		filter: (addonTestCase) => addonTestCase.variant.includes('kit')
 	}
 );
 
-test.concurrent.for(testCases)('paraglide $variant', (testCase, { ...ctx }) => {
+test.concurrent.for(testCases)('paraglide $variant', async (testCase, { page, ...ctx }) => {
 	const cwd = ctx.cwd(testCase);
+
+	const { close } = await prepareServer({ cwd, page });
+	// kill server process when we're done
+	ctx.onTestFinished(async () => await close());
 
 	for (const lang of langs) {
 		const filePath = path.resolve(cwd, `src/lib/paraglide/messages/${lang}.js`);
