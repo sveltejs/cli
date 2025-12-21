3e2362ec394635a5facc90743dd2a96eeaed3044
jycouet <jycouet@gmail.com>
2025-12-21 13:37:59 +0100

    add parser

--- packages/sv/lib/core.ts
+++ packages/sv/lib/core.ts
@@ -4,6 +4,8 @@ export { default as dedent } from 'dedent';
 export { defineAddon, defineAddonOptions } from './core/addon/config.ts';
 export { isVersionUnsupportedBelow, splitVersion } from './core/common.ts';
 
+export { fileExists } from './cli/add/utils.ts';
+
 export * as utils from './core/utils.ts';
 export type * from './core/addon/processors.ts';
 export type * from './core/addon/options.ts';
@@ -20,6 +22,7 @@ import {
 	parseJson,
 	parseScript,
 	parseSvelte,
+	parseToml,
 	parseYaml
 } from './core/tooling/parsers.ts';
 export const parse = {
@@ -28,6 +31,7 @@ export const parse = {
 	json: parseJson as typeof parseJson,
 	script: parseScript as typeof parseScript,
 	svelte: parseSvelte as typeof parseSvelte,
+	toml: parseToml as typeof parseToml,
 	yaml: parseYaml as typeof parseYaml
 };
 
