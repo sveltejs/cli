75632600ac5f288c25782e1b5015116cb6bea080
jycouet <jycouet@gmail.com>
2025-12-21 13:42:22 +0100

    add todo

--- packages/sv/lib/core.ts
+++ packages/sv/lib/core.ts
@@ -1,17 +1,21 @@
+// TODO JYC: audit these exports... It's our public API!
 export { log } from '@clack/prompts';
 export { default as dedent } from 'dedent';
 
 export { defineAddon, defineAddonOptions } from './core/addon/config.ts';
 export { isVersionUnsupportedBelow, splitVersion } from './core/common.ts';
-
 export { fileExists } from './cli/add/utils.ts';
+export { color } from './cli/add/utils.ts';
+export { resolveCommand } from 'package-manager-detector/commands';
+export { getNodeTypesVersion, addToDemoPage, addEslintConfigPrettier } from './addons/common.ts';
+export { getSharedFiles } from './create/utils.ts';
 
-export * as utils from './core/utils.ts';
 export type * from './core/addon/processors.ts';
 export type * from './core/addon/options.ts';
 export type * from './core/addon/config.ts';
 export type * from './core/addon/workspace.ts';
 
+export * as utils from './core/utils.ts';
 export * as css from './core/tooling/css/index.ts';
 export * as js from './core/tooling/js/index.ts';
 export * as svelte from './core/tooling/svelte/index.ts';
@@ -38,7 +42,3 @@ export const parse = {
 export { Walker } from './core/tooling/index.ts';
 export type { Comments, AstTypes } from './core/tooling/index.ts';
 export type { SvelteAst } from './core/tooling/svelte/index.ts';
-export { resolveCommand } from 'package-manager-detector/commands';
-export { getNodeTypesVersion, addToDemoPage, addEslintConfigPrettier } from './addons/common.ts';
-export { getSharedFiles } from './create/utils.ts';
-export { color } from './cli/add/utils.ts';
