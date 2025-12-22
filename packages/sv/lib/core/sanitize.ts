126932d8979e4e190fe2a30f9874ecedb34c60c9
jycouet <jycouet@gmail.com>
2025-12-22 18:39:07 +0100

    like this

--- packages/sv/lib/core/sanitize.ts
+++ packages/sv/lib/core/sanitize.ts
@@ -1,27 +1,27 @@
 /**
  * @param name - The name to sanitize.
- * @param kind - The kind of name to sanitize.
+ * @param style - The kind of name to sanitize.
  *   - `package` for package.json
  *   - `wrangler` for Cloudflare Wrangler compatibility
  * @returns The sanitized name.
  */
-export function sanitizeName(name: string, kind: 'package' | 'wrangler'): string {
+export function sanitizeName(name: string, style: 'package' | 'wrangler'): string {
 	let sanitized = name.trim().toLowerCase().replace(/\s+/g, '-');
-	if (kind === 'package') {
+	if (style === 'package') {
 		const hasLeadingAt = sanitized.startsWith('@');
 		sanitized = sanitized
 			.replace(/\s+/g, '-')
 			.replace(/^[._]/, '')
 			.replace(/[^a-z0-9~./-]+/g, '-');
 		if (hasLeadingAt) sanitized = '@' + sanitized.slice(1);
-	} else if (kind === 'wrangler') {
+	} else if (style === 'wrangler') {
 		sanitized = sanitized
 			.replace(/[^a-z0-9-]/g, '-')
 			.replace(/-+/g, '-')
 			.slice(0, 63)
 			.replace(/^-|-$/g, '');
 	} else {
-		throw new Error(`Invalid kind: ${kind}`);
+		throw new Error(`Invalid kind: ${style}`);
 	}
 	return sanitized || 'undefined-sv-name';
 }
