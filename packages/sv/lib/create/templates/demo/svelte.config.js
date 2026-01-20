051acb8ba4c6b3e1ae559999d2d43434fc46c79b
jyc.dev <jycouet@gmail.com>
2026-01-20 02:28:27 +0100

    chore(cli): remove `vitePreprocess` in all default templates (#876)

--- packages/sv/lib/create/templates/demo/svelte.config.js
+++ packages/sv/lib/create/templates/demo/svelte.config.js
@@ -1,14 +1,9 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 // This config is ignored and replaced with one of the configs in the shared folder when a project is created.
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
 	kit: {
 		adapter: adapter()
 	}
