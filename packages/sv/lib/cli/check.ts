c056bb0a063f6d709085a800276ecb04740abb40
jyc.dev <jycouet@gmail.com>
2026-01-31 09:56:52 +0100

    fix(cli): unified package manager detection (#900)

--- packages/sv/lib/cli/check.ts
+++ packages/sv/lib/cli/check.ts
@@ -5,7 +5,7 @@ import process from 'node:process';
 import { resolveCommand } from 'package-manager-detector/commands';
 
 import { forwardExitCode } from './utils/common.ts';
-import { getUserAgent } from './utils/package-manager.ts';
+import { detectPackageManager } from './utils/package-manager.ts';
 import { color } from '../core.ts';
 
 export const check = new Command('check')
@@ -14,22 +14,16 @@ export const check = new Command('check')
 	.allowUnknownOption(true)
 	.allowExcessArguments(true)
 	.option('-C, --cwd <path>', 'path to working directory', process.cwd())
-	.configureHelp({
-		formatHelp() {
-			// we'll pass the responsibility of presenting the help menu over to `svelte-check`
-			runCheck(process.cwd(), ['--help']);
-			return '';
-		}
-	})
-	.action((options, check: Command) => {
+	.helpOption(false) // we'll pass the responsibility of presenting the help menu over to `svelte-check`
+	.action(async (options, check: Command) => {
 		const cwd: string = options.cwd;
 		const args: string[] = check.args;
 
-		runCheck(cwd, args);
+		await runCheck(cwd, args);
 	});
 
-function runCheck(cwd: string, args: string[]) {
-	const pm = getUserAgent() ?? 'npm';
+async function runCheck(cwd: string, args: string[]) {
+	const pm = await detectPackageManager(cwd);
 
 	// validates that `svelte-check` is locally installed
 	const resolved = resolve.from(cwd, 'svelte-check', true);
@@ -41,11 +35,20 @@ function runCheck(cwd: string, args: string[]) {
 		process.exit(1);
 	}
 
+	if (args.includes('--help')) {
+		console.log(`All svelte-check [options] are available in sv check [options]`);
+		console.log('Find here all options for both tools');
+	}
+
 	// avoids printing the stack trace for `sv` when `svelte-check` exits with an error code
 	try {
 		const cmd = resolveCommand(pm, 'execute-local', ['svelte-check', ...args])!;
 		execSync(`${cmd.command} ${cmd.args.join(' ')}`, { stdio: 'inherit', cwd });
 	} catch (error) {
 		forwardExitCode(error);
+	} finally {
+		if (args.includes('--help')) {
+			check.help();
+		}
 	}
 }
