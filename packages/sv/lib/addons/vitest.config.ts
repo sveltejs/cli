f7781f3171524cc9b27a076488ac83b3d83e4274
jycouet <jycouet@gmail.com>
2026-02-06 22:59:40 +0100

    step 11

--- packages/sv/lib/addons/vitest.config.ts
+++ packages/sv/lib/addons/vitest.config.ts
@@ -6,8 +6,8 @@ const ONE_MINUTE = 1000 * 60;
 export default defineProject({
 	test: {
 		name: 'addons',
-		include: ['_tests/**/test.{js,ts}'],
-		globalSetup: ['_tests/_setup/global.ts'],
+		include: ['tests/**/test.{js,ts}'],
+		globalSetup: ['tests/_setup/global.ts'],
 		testTimeout: ONE_MINUTE * 3,
 		hookTimeout: ONE_MINUTE * 3,
 		retry: env.CI ? 3 : 0,
