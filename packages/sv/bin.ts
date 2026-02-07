2fd2ae93394bf794fae53c420e783240d079ff80
jycouet <jycouet@gmail.com>
2026-02-07 13:08:07 +0100

    fmt

--- packages/sv/bin.ts
+++ packages/sv/bin.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import pkg from './package.json' with { type: 'json' };
 import { add } from './src/cli/add.ts';
 import { check } from './src/cli/check.ts';
 import { create } from './src/cli/create.ts';
 import { migrate } from './src/cli/migrate.ts';
 import { helpConfig } from './src/core/common.ts';
-import pkg from './package.json' with { type: 'json' };
 
 // adds a gap of spacing between the executing command and the output
 console.log();
