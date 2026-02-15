08b7ab9989bd8b4278225e304075e409fe4c5555
jyc.dev <jycouet@gmail.com>
2026-02-15 17:29:36 +0100

    chore: bump svelte to `5.51` (#915)
    * chrore 5.50
    
    * add frag
    
    * back
    
    * 5.51
    
    * pnpm i
    
    * add changelogs

---
+++ .changeset/brown-boats-fly.md
@@ -0,0 +1,5 @@
+---
+'sv': patch
+---
+
+chore(cli): bump svelte to `5.51` to fix formating issues
--- packages/sv/lib/cli/tests/snapshots/create-with-all-addons/src/routes/+layout.svelte
+++ packages/sv/lib/cli/tests/snapshots/create-with-all-addons/src/routes/+layout.svelte
@@ -8,14 +8,12 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-
 {@render children()}
+
 <div style="display:none">
 	{#each locales as locale}
 		<a
 			href={localizeHref(page.url.pathname, { locale })}
-		>
-			{locale}
-		</a>
+		>{locale}</a>
 	{/each}
 </div>
