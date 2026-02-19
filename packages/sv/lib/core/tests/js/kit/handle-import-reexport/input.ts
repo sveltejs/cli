229ec47d0bc9382268ff930034cd225b1ab422f8
jycouet <jycouet@gmail.com>
2026-02-19 11:09:04 +0100

    format

--- packages/sv/lib/core/tests/js/kit/handle-import-reexport/input.ts
+++ packages/sv/lib/core/tests/js/kit/handle-import-reexport/input.ts
@@ -1,4 +1,4 @@
-import { i18n } from '$lib/i18n';
 import { handle } from '$lib/auth.server';
+import { i18n } from '$lib/i18n';
 
 export { handle };
--- packages/sv/lib/core/tests/js/kit/handle-import-reexport/output.ts
+++ packages/sv/lib/core/tests/js/kit/handle-import-reexport/output.ts
@@ -1,7 +1,7 @@
 import { sequence } from '@sveltejs/kit/hooks';
 import type { Handle } from '@sveltejs/kit';
-import { i18n } from '$lib/i18n';
 import { handleExisting } from '$lib/auth.server';
+import { i18n } from '$lib/i18n';
 
 const handleFoo: Handle = i18n.handle();
 
