02e855e40a262826584fc0bedc0497f47f431329
jyc.dev <jycouet@gmail.com>
2026-02-20 19:43:24 +0100

    chore: improve windows testing speed (#933)
    * improve windows testing
    
    * I HATE PRETTIER AND VS CODE SETUP
    
    * remove all addon test
    
    ---------
    
    Co-authored-by: Manuel Serret <mserret99@gmail.com>

--- packages/sv/src/addons/vitest.config.ts
+++ packages/sv/src/addons/vitest.config.ts
@@ -1,15 +1,16 @@
-import { env } from 'node:process';
+import { env, platform } from 'node:process';
 import { defineProject } from 'vitest/config';
 
 const ONE_MINUTE = 1000 * 60;
+const isWindows = platform === 'win32';
 
 export default defineProject({
 	test: {
 		name: 'addons',
 		include: ['tests/**/test.{js,ts}'],
 		globalSetup: ['tests/_setup/global.ts'],
-		testTimeout: ONE_MINUTE * 3,
-		hookTimeout: ONE_MINUTE * 3,
+		testTimeout: ONE_MINUTE * (isWindows ? 5 : 3),
+		hookTimeout: ONE_MINUTE * (isWindows ? 5 : 3),
 		retry: env.CI ? 3 : 0,
 		expect: {
 			requireAssertions: true
