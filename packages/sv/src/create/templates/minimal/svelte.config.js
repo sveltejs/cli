b169029a5b80360416d9a2f1a28f0f16e39f8644
Scott Wu <sw@scottwu.ca>
2026-02-27 20:55:29 +0800

    enable rune mode

--- packages/sv/src/create/templates/minimal/svelte.config.js
+++ packages/sv/src/create/templates/minimal/svelte.config.js
@@ -6,6 +6,9 @@ import adapter from '@sveltejs/adapter-auto';
 const config = {
 	kit: {
 		adapter: adapter()
+	},
+	vitePlugin: {
+		dynamicCompileOptions: ({ filename }) => ({ runes: !filename.includes('node_modules') })
 	}
 };
 
