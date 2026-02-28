28c92caea48a49cf6cd312be4f5065c197892dcf
Scott Wu <sw@scottwu.ca>
2026-02-28 11:16:35 +0800

    update snapshots

--- packages/sv/src/cli/tests/snapshots/create-with-all-addons/svelte.config.js
+++ packages/sv/src/cli/tests/snapshots/create-with-all-addons/svelte.config.js
@@ -4,6 +4,9 @@ import adapter from '@sveltejs/adapter-node';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: { adapter: adapter() },
+	vitePlugin: {
+		dynamicCompileOptions: ({ filename }) => ({ runes: !filename.includes('node_modules') })
+	},
 	preprocess: [mdsvex()],
 	extensions: ['.svelte', '.svx']
 };
