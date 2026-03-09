68cfa6d4961e00cf9248625201941d25edd95f48
Scott Wu <sw@scottwu.ca>
2026-03-10 01:35:42 +0800

    adjust for js

--- packages/sv/src/addons/eslint.ts
+++ packages/sv/src/addons/eslint.ts
@@ -53,7 +53,7 @@ export default defineAddon({
 			}
 
 			const svelteConfig = js.common.parseExpression('svelte.configs.recommended');
-			eslintConfigs.push(svelteConfig);
+			eslintConfigs.push(typescript ? svelteConfig : js.common.createSpread(svelteConfig));
 
 			const globalsBrowser = js.common.createSpread(js.common.parseExpression('globals.browser'));
 			const globalsNode = js.common.createSpread(js.common.parseExpression('globals.node'));
@@ -156,7 +156,7 @@ export default defineAddon({
 		});
 
 		if (prettierInstalled) {
-			sv.file(files.eslintConfig, addEslintConfigPrettier);
+			sv.file(files.eslintConfig, (content) => addEslintConfigPrettier(content, language));
 		}
 	}
 });
