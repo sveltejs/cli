5cf16c1b0efdf16379f578360b3ea0a1f81815dd
Scott Wu <sw@scottwu.ca>
2026-03-17 18:27:24 +0800

    add snapshot

--- packages/sv/src/cli/tests/snapshots/create-with-all-addons/svelte.config.js
+++ packages/sv/src/cli/tests/snapshots/create-with-all-addons/svelte.config.js
@@ -1,11 +1,24 @@
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-node';
+import { relative, sep } from 'node:path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: { adapter: adapter() },
 	vitePlugin: {
-		dynamicCompileOptions: ({ filename }) => filename.includes('node_modules') ? undefined : { runes: true }
+		// ensures rune mode is used by default, but allows for dependencies to use legacy mode
+		// to be removed in svelte 6
+		dynamicCompileOptions: ({ filename }) => {
+			const relativePath = relative(import.meta.dirname, filename);
+			const pathSegments = relativePath.toLowerCase().split(sep);
+			const isExternalLibrary = pathSegments.includes('node_modules');
+
+			if (isExternalLibrary) {
+				return undefined;
+			} else {
+				return { runes: true };
+			}
+		}
 	},
 	preprocess: [mdsvex()],
 	extensions: ['.svelte', '.svx']
