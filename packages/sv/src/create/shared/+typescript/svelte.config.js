8126f9a76a459a6f65812ae5aebc5211636c792d
Scott Wu <sw@scottwu.ca>
2026-03-18 21:59:25 +0800

    use `compilerOptions`

--- packages/sv/src/create/shared/+typescript/svelte.config.js
+++ packages/sv/src/create/shared/+typescript/svelte.config.js
@@ -3,26 +3,21 @@ import { relative, sep } from 'node:path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	compilerOptions: {
+		// defaults to rune mode for the project, execept for `node_modules`. Can be removed in svelte 6.
+		runes: ({ filename }) => {
+			const relativePath = relative(import.meta.dirname, filename);
+			const pathSegments = relativePath.toLowerCase().split(sep);
+			const isExternalLibrary = pathSegments.includes('node_modules');
+
+			return isExternalLibrary ? undefined : true;
+		}
+	},
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
-	},
-	vitePlugin: {
-		// ensures rune mode is used by default, but allows for dependencies to use legacy mode
-		// to be removed in svelte 6
-		dynamicCompileOptions: ({ filename }) => {
-			const relativePath = relative(import.meta.dirname, filename);
-			const pathSegments = relativePath.toLowerCase().split(sep);
-			const isExternalLibrary = pathSegments.includes('node_modules');
-
-			if (isExternalLibrary) {
-				return undefined;
-			} else {
-				return { runes: true };
-			}
-		}
 	}
 };
 
