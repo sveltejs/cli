1b787e45713652ced9dddff265411bd2a7780866
Scott Wu <sw@scottwu.ca>
2026-04-02 10:55:46 +0800

    refactor

--- packages/sv-utils/src/color.ts
+++ packages/sv-utils/src/color.ts
@@ -1,24 +1,25 @@
 import { styleText } from 'node:util';
 
-type ColorInput = string | string[];
-const toStr = (input: ColorInput): string => (Array.isArray(input) ? input.join(' ') : input);
+type WithFormat = (format: Parameters<typeof styleText>[0]) => (input: string | string[]) => string;
+const withFormat: WithFormat = (format) => (input) =>
+	styleText(format, Array.isArray(input) ? input.join(' ') : input);
 
 export const color = {
 	// Semantic colors
-	addon: (str: ColorInput): string => styleText('greenBright', toStr(str)),
-	command: (str: ColorInput): string => styleText(['bold', 'cyanBright'], toStr(str)),
-	env: (str: ColorInput): string => styleText('yellow', toStr(str)),
-	path: (str: ColorInput): string => styleText('blueBright', toStr(str)),
-	route: (str: ColorInput): string => styleText(['bold', 'underline'], toStr(str)),
-	website: (str: ColorInput): string => styleText('cyan', toStr(str)),
-	optional: (str: ColorInput): string => styleText('gray', toStr(str)),
-	dim: (str: ColorInput): string => styleText(['gray', 'dim'], toStr(str)), // needed for terminal that don't support `dim` well
+	addon: withFormat('greenBright'),
+	command: withFormat(['bold', 'cyanBright']),
+	env: withFormat('yellow'),
+	path: withFormat('blueBright'),
+	route: withFormat(['bold', 'underline']),
+	website: withFormat('cyan'),
+	optional: withFormat('gray'),
+	dim: withFormat(['gray', 'dim']), // needed for terminal that don't support `dim` well
 
 	// Status colors
-	success: (str: ColorInput): string => styleText('green', toStr(str)),
-	warning: (str: ColorInput): string => styleText('yellow', toStr(str)),
-	error: (str: ColorInput): string => styleText('red', toStr(str)),
+	success: withFormat('green'),
+	warning: withFormat('yellow'),
+	error: withFormat('red'),
 
 	// Visibility
-	hidden: (str: ColorInput): string => styleText('hidden', toStr(str))
+	hidden: withFormat('hidden')
 };
