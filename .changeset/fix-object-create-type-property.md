bafb873dea18e4a66468c1d958d48167fe0b71f2
jycouet <jycouet@gmail.com>
2026-05-05 21:40:27 +0200

    fix: `js.object.create` no longer corrupts the AST when an object value contains a `type` property

---
+++ .changeset/fix-object-create-type-property.md
@@ -0,0 +1,5 @@
+---
+'@sveltejs/sv-utils': patch
+---
+
+fix: `js.object.create` no longer corrupts the AST when an object value contains a `type` property
--- packages/sv-utils/src/tests/js/object/create/output.ts
+++ packages/sv-utils/src/tests/js/object/create/output.ts
@@ -7,3 +7,9 @@ const created2 = {
 	object: { foo: 'hello', nested: { bar: 'world' } },
 	array: [123, 'hello', { foo: 'bar', bool: true }, [456, '789']]
 };
+
+const created3 = {
+	type: '123',
+	nested: { type: 'something', value: 'inside' },
+	array: [{ type: 'item', id: 1 }]
+};
