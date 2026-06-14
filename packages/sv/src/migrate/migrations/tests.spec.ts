0e4882ed90bc1438d4c2ab921ffa2986055dcf58
Manuel Serret <mserret99@gmail.com>
2026-06-14 17:45:05 +0200

    testing (part 5/x)

--- packages/sv/src/migrate/migrations/tests.spec.ts
+++ packages/sv/src/migrate/migrations/tests.spec.ts
@@ -9,6 +9,12 @@ const baseDir = resolve(fileURLToPath(import.meta.url), '..');
 const migrationDirectories = getDirectoryNames(baseDir);
 const tasksDirectoryName = 'tasks';
 const testsDirectoryName = 'tests';
+const defaultDependencies = {
+	vite: '^8.0.0',
+	'@sveltejs/vite-plugin-svelte': '^7.0.0',
+	svelte: '^5.48.0',
+	typescript: '^6.0.0' // TS projects only
+};
 
 for (const migrationDirectory of migrationDirectories) {
 	const testNames = getFileNames(path.join(baseDir, migrationDirectory, tasksDirectoryName));
@@ -18,18 +24,16 @@ for (const migrationDirectory of migrationDirectories) {
 		for (const testName of testNames) {
 			test(testName, async () => {
 				const cwd = path.join(baseDir, migrationDirectory, testsDirectoryName, testName);
+				copyTemplateFiles(cwd);
 
 				const workspace = await createWorkspace({
 					cwd,
 					packageManager: 'pnpm',
-					override: {
-						dependencies: {
-							vite: '^8.0.0',
-							'@sveltejs/vite-plugin-svelte': '^7.0.0',
-							svelte: '^5.48.0',
-							typescript: '^6.0.0' // TS projects only
-						}
-					}
+
+					// only add the default dependencies if there is no package.json
+					...(fs.existsSync(path.join(cwd, 'package.json'))
+						? {}
+						: { override: { dependencies: defaultDependencies } })
 				});
 				const modifiedFiles = new Set<string>();
 				const { sv, updateDependencies } = prepareSvApi(workspace, modifiedFiles, {
@@ -53,6 +57,25 @@ for (const migrationDirectory of migrationDirectories) {
 			});
 		}
 	});
+}
+
+/**
+ * Some files might be deleted by migrations, but required to test the migration each time.
+ * That's why we keep those template files which are copied before each test run
+ * and can be used for the migration to wor on.
+ */
+function copyTemplateFiles(dir: string) {
+	if (!fs.existsSync(dir)) return;
+
+	const templateFiles = fs.globSync('**/*.template.*', {
+		cwd: dir,
+		exclude: ['node_modules/**', '**/node_modules/**']
+	});
+
+	for (const templateFile of templateFiles) {
+		const targetFile = templateFile.replace('.template.', '.');
+		fs.copyFileSync(path.join(dir, templateFile), path.join(dir, targetFile));
+	}
 }
 
 function getFileNames(dir: string) {
