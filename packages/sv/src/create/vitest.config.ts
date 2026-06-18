772bbcf692c533ded32efa7a77b21c97abab9501
Manuel Serret <mserret99@gmail.com>
2026-06-18 23:10:41 +0200

    more create timeout?

--- packages/sv/src/create/vitest.config.ts
+++ packages/sv/src/create/vitest.config.ts
@@ -1,10 +1,14 @@
 import { env } from 'node:process';
 import { defineProject } from 'vitest/config';
 
+const ONE_MINUTE = 1000 * 60;
+
 export default defineProject({
 	test: {
 		name: 'create',
 		include: ['tests/*.ts'],
+		hookTimeout: ONE_MINUTE * 3,
+		testTimeout: ONE_MINUTE * 3,
 		retry: env.CI ? 3 : 0
 	}
 });
