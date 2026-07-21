d83660067d8f67ab5bb4f70d9f0f225bac792e8b
Manuel Serret <mserret99@gmail.com>
2026-07-21 21:40:46 +0200

    maybe with retries?

--- packages/sv/src/cli/vitest.config.ts
+++ packages/sv/src/cli/vitest.config.ts
@@ -1,3 +1,4 @@
+import { env } from 'node:process';
 import { defineProject } from 'vitest/config';
 
 export default defineProject({
@@ -5,6 +6,7 @@ export default defineProject({
 		name: 'cli',
 		include: ['./tests/**/index.ts', './tests/*.ts'],
 		exclude: ['./tests/snapshots/**'],
+		retry: env.CI ? 3 : 0,
 		expect: {
 			requireAssertions: true
 		}
