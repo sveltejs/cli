e1892a4b22aa96b807f851e22fd5d4edb5d7296d
Scott Wu <sw@scottwu.ca>
2026-08-01 04:01:11 +0800

    snapshot

--- packages/sv/src/cli/tests/snapshots/create-with-all-addons/src/routes/addon/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import { resolve } from '$app/paths';
-</script>
-
-<a href={resolve('/addon/better-auth')}>better-auth</a>
-<a href={resolve('/addon/paraglide')}>paraglide</a>
-<a href={resolve('/addon/playwright')}>playwright</a>
