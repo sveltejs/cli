c21ade03e9c7ff42fcd277c6a05115a16846fa1a
Manuel Serret <mserret99@gmail.com>
2026-08-07 09:01:21 +0200

    more fixes

--- packages/sv/src/cli/tests/snapshots/create-with-all-addons/src/routes/+layout.svelte
+++ packages/sv/src/cli/tests/snapshots/create-with-all-addons/src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import type { Pathname } from '$app/types';
+	import type { Path } from '$app/types';
 	import { resolve } from '$app/paths';
 	import { page } from '$app/state';
 	import { locales, localizeHref } from '#lib/paraglide/runtime';
@@ -18,7 +18,7 @@
 <div style="display:none">
 	{#each locales as locale (locale)}
 		<a
-			href={resolve(localizeHref(page.url.pathname, { locale }) as Pathname)}
+			href={resolve(localizeHref(page.url.pathname, { locale }) as Path)}
 		>{locale}</a>
 	{/each}
 </div>
