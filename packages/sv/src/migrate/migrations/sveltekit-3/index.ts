e6fa26ee60594e6c418c7e846dbb2b9da99b32ec
Simon H <5968653+dummdidumm@users.noreply.github.com>
2026-08-07 14:38:47 +0200

    feat: external redirects migration (#1228)
    For SvelteKit 3
    
    Co-authored-by: Manuel Serret <mserret99@gmail.com>

--- packages/sv/src/migrate/migrations/sveltekit-3/index.ts
+++ packages/sv/src/migrate/migrations/sveltekit-3/index.ts
@@ -2,6 +2,7 @@ import { coerceVersion, color } from '@sveltejs/sv-utils';
 import { defineMigration } from '../../index.ts';
 import appState from '../app-state/tasks/app-state.ts';
 import environment from './tasks/environment.ts';
+import externalRedirects from './tasks/external-redirects.ts';
 import packageJson from './tasks/package-json.ts';
 import params from './tasks/params.ts';
 import shallowRouting from './tasks/shallow-routing.ts';
@@ -30,6 +31,7 @@ export default defineMigration({
 		tasks.add(packageJson, { prerequisite: true });
 		tasks.add(svelteConfig, { prerequisite: false });
 		tasks.add(environment, { prerequisite: false });
+		tasks.add(externalRedirects, { prerequisite: false });
 		tasks.add(shallowRouting, { prerequisite: false });
 		tasks.add(params, { prerequisite: false });
 		tasks.add(tsconfig, { prerequisite: true });
