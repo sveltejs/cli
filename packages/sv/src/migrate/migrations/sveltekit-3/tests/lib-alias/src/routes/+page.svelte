e4ef89ac53f2a461de3e817fe6a607110c776276
Simon Holthausen <simon.holthausen@vercel.com>
2026-08-11 18:10:41 +0200

    add file extensions to `#lib` when migrating

--- packages/sv/src/migrate/migrations/sveltekit-3/tests/lib-alias/src/routes/+page.svelte
+++ packages/sv/src/migrate/migrations/sveltekit-3/tests/lib-alias/src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <script>
 	import component from '$lib/component.svelte';
+	import file from '$lib/file';
+	import ts from '$lib/ts';
+	import index from '$lib/index';
+	import alreadySpecifier from '$lib/alreadySpecifier.js';
 	import { library } from '$library';
 </script>
 
