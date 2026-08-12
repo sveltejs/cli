9b09c1c3280ca9846d77f3435141ec6dc25a1c23
Manuel Serret <mserret99@gmail.com>
2026-08-12 17:40:15 +0200

    fix

--- packages/sv/src/migrate/migrations/sveltekit-3/tasks/svelte-config.ts
+++ packages/sv/src/migrate/migrations/sveltekit-3/tasks/svelte-config.ts
@@ -7,8 +7,6 @@ import {
 	type AstTypes,
 	type Comments
 } from '@sveltejs/sv-utils';
-import fs from 'node:fs';
-import path from 'node:path';
 import { defineMigrationTask } from '../../../index.ts';
 import { addMigrationTask } from '../../../migration-task.ts';
 
